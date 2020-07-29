3a930d8b54465e6d1d88e9511e2fa55120e16472
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 16:38:09 -0500

    [Feature] Sum and Average

--- CProgramControl/fournine.c
+++ CProgramControl/fournine.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
 
-/*
-* (Sum and Average of Integers) Write a program to sum a sequence of integers and calcu-
-* late their average. Assume that the first integer read with scanf specifies the number of values to
-* be entered. Your program should read only one value each time scanf is executed. A typical input
-* sequence might be
-*/
 
 /**
- * main - Print the average of the numbers 
- * Return: Integer if is succesful
- */
-
+ * main - (Sum and Average of Integers)
+ *
+ * Write a program to sum a sequence of integers and calculate their average.
+ * Assume that the first integer read with scanf
+ * specifies the number of values to be entered.
+ * Your program should read only one value each time scanf is executed.
+ * A typical input sequence might be
+ *
+ * Return: 0 if is success
+*/
 int main(void)
 {
-	int number, result, sentinel, count;
- 
-	number = 0;
-	result = 0;
-	sentinel = 1;
-	count = 0;
+        int number = 0, result = 0, sentinel = 1, count = 0;
 
-	while (sentinel)
-	{
-		printf("Enter your number: ");
-		scanf("%d", &number);
+        while (sentinel)
+        {
+                printf("Enter your number: ");
+                scanf("%d", &number);
 
-		if (number == -1)
-		{
-			sentinel = -1;
-			break;
-		}
-		else
-		{
-			result += number;
-			++count;
-		}
-	}
-	printf("The average is: %d\n", (result / count));
+                if (number == -1)
+                {
+                        sentinel = -1;
+                        break;
+                }
+                else
+                {
+                        result += number;
+                        ++count;
+                }
+        }
+        printf("The average is: %d\n", (result / count));
 
-	return (0);
+        return (0);
 }
