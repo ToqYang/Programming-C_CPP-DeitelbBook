cbe2bfeb58ce95fcd91b6efea28cb67b69c2bbc3
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 18:26:32 -0500

    [Feature] ASCII values

--- CProgramControl/fourtwentyone.c
+++ CProgramControl/fourtwentyone.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
 
-/*
- * (ASCII Values) Write a program to convert and print the characters for the ASCII values
- * 0 to 127. The program should print 10 characters per line.
- */
 
-/* 
- * main - Print the characters of the 0 at the 127
+/**
+ * main -(ASCII Values)
+ *
+ * Write a program to convert and print the characters for the ASCII values
+ * 0 to 127. The program should print 10 characters per line.
+ *
  * Return: 0 if is success
  */
-
 int main(void)
 {
-	int line = 0, limit = 127;
+        int limit = 127;
 
-	for ( ; line <= limit; ++line)
-	{
-		/* Print the character ascii */
-		putchar(line);
-		putchar(32);
+        for (int line = 0; line <= limit; ++line)
+        {
+                /* Print the character ascii */
+                putchar(line);
+                putchar(32);
 
-		if (line % 10 == 0)
-			putchar(10);
+                if (line % 10 == 0)
+                        putchar(10);
+        }
+        putchar(10);
 
-	}
-	putchar(10);
-
-	return (0);
+        return (0);
 }
