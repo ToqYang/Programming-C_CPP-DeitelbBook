90734a1c1b388d9c3627fafbeb7ddd64b1678ccf
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 15:13:52 -0500

    [Feature]  Square asterisk 1-20

--- StructuredProgramming/threethirtytwo.c
+++ StructuredProgramming/threethirtytwo.c
@@ -1,40 +1,36 @@
 #include <stdio.h>
 
+
 /**
- * (Square of Asterisks) Write a program that reads in the side of a square and then prints thatsquare out of asterisks. 
+ * main - (Square of Asterisks)
+ *
+ * Write a program that reads in
+ * the side of a square and then prints thatsquare out of asterisks.
  * Your program should work for squares of all side sizes between 1 and 20.
  * For example, if your program reads a size of 4, it should print
+ *
+ * Return: 0 if is success
  */
-
-/**
- * main - Print square of asterisks size
- * Return: 0
- */
-
-int main( void )
+int main(void)
 {
-	int rows, column, size;
+        int rows = 0, column = 0, size = 0;
 
-	rows = 0;
-	column = 0;
-	size = 0;
+        printf("Enter the size: ");
+        scanf("%d", &size);
 
-	printf( "Enter the size: " );
-	scanf( "%d", &size );
+        while (column < size)
+        {
+                rows = 0;
 
-	while ( column < size )
-	{
-		rows = 0;
+                while (rows < size)
+                {
+                        putchar(42);
+                        rows++;
+                }
 
-		while ( rows < size )
-		{
-			printf( "*" );
-			rows++;
-		}
+                putchar(10);
+                column++;
+        }
 
-		printf( "\n" );
-		column++;
-	}
-
-	return (0);
+        return (0);
 }
