c35fa4de5d43934be032a0ddd126f293fdaa2038
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 18:31:50 -0500

    [Feature]  isSidesCorrect

--- CProgramControl/fourtwentyseven.c
+++ CProgramControl/fourtwentyseven.c
@@ -1,48 +1,46 @@
 #include <stdio.h>
 #include <math.h>
 
-/**
- * (Pythagorean Triples)
- */
 
 /**
- * main - Print if the three sides are corrects and C <= 500
- * Return: Print of th sides a, b, c
+ * main - (Pythagorean Triples)
+ *
+ * if the three sides are corrects and C <= 500
+ *
+ * Return: 0 if is success
  */
-
 int main(void)
 {
-	int a1 = 0, b1 = 0, c1 = 0; 
-	int a = 0, b = 0, c = 0;
-	int side1 = 0, side2 = 0, side3 = 0;
+        int a1 = 0, b1 = 0, c1 = 0;
+        int a = 0, b = 0, c = 0;
+        int side1 = 0, side2 = 0, side3 = 0;
 
-	for ( ; a1 != -1 || b1 != -1 || c1 != -1 ; )
-	{
-		puts("Enter your numbers");
-		scanf("%d %d %d", &a1, &b1, &c1);
+        while (a1 != -1 || b1 != -1 || c1 != -1) {
+                puts("Enter your numbers");
+                scanf("%d %d %d", &a1, &b1, &c1);
 
-		/** If there negative numbers */ 
-		if (a1 == -1 || b1 == -1 || c1 == -1)
-			return(1);
+                /* If there negative numbers */
+                if (a1 == -1 || b1 == -1 || c1 == -1)
+                        return (1);
 
-		/** Verify if the sides are corrects and the
-		 * hypotenuse is <= 500
+                /* Verify if the sides are corrects and the
+		   hypotenuse is <= 500
 		 */
-		for ( ; (side1 + side2) == side3 && side3 <= 500 ; )  
-		{
-			/** Step for take the constan and sum to get the family*/
-			a += a1;
-			b += b1;
-			c += c1;
+                while ((side1 + side2) == side3 && side3 <= 500) {
+                        /* Step for take the constan and sum to get the family*/
+                        a += a1;
+                        b += b1;
+                        c += c1;
 
-			side1 = pow(a, 2);
-			side2 = pow(b, 2);
-			side3 = pow(c, 2);
+                        side1 = pow(a, 2);
+                        side2 = pow(b, 2);
+                        side3 = pow(c, 2);
 
-			/** Print if the condition o the loop is correct */
-			if ((side1 + side2) == side3 && side3 <= 500)
-				printf("\na^2 = %d, b^2 = %d c^2 = %d^2\n", a, b, c);
-		}
-	}
-	return (0);
+                        /* Print if the condition o the loop is correct */
+                        if ((side1 + side2) == side3 && side3 <= 500)
+                                printf("\na^2 = %d, b^2 = %d c^2 = %d^2\n",
+                                       a, b, c);
+                }
+        }
+        return (0);
 }
