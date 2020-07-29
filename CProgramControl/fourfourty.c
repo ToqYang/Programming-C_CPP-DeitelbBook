23e34a9f054afd5c854d81002227d3a3e129793f
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 16:32:54 -0500

    [Feature] World Population in the future

--- CProgramControl/fourfourty.c
+++ CProgramControl/fourfourty.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 
+
 /**
- * World-Population-Growth Calculator)
+ * main - (World-Population-Growth Calculator)
+ *
  * Print the suposition of 1-75 years the World Population
  * The values of the people is in millions
  * Total population is 7.500 millions
  * Annual growth is of 80 millions
+ *
  * Return: 0 If, is successful
  */
-
-int main ( void )
+int main(void)
 {
-	int pop_Anual = 80, pop_Total = 7500, years = 1, pop_Show = 0;
+        int pop_Anual = 80, pop_Total = 7500, years = 1, pop_Show = 0;
 
-	puts("Year  Population end year  Mensual growth\n");
+        puts("Year  Population end year  Mensual growth\n");
 
-	while ( years <= 75 )
-	{
-		pop_Show = 0;
+        while (years <= 75)
+        {
+                pop_Show = 0;
 
-		/* Calculate the anual populstion */
-		pop_Show += ( pop_Total + ( pop_Anual * years) );
+                /* Calculate the anual populstion */
+                pop_Show += (pop_Total + (pop_Anual * years));
 
-		printf("%d\t\t%d\t\t%d\n", years, pop_Show, pop_Anual / 12);
+                printf("%d\t\t%d\t\t%d\n", years, pop_Show, pop_Anual / 12);
 
-		++years;
-	}
+                ++years;
+        }
 
-	return (0);
+        return (0);
 }
