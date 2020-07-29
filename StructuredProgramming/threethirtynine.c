f48e5dfeaefef176197c58f6b58c6401d4f57a92
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 13:52:04 -0500

    [Feature] Asterisks

--- StructuredProgramming/threethirtynine.c
+++ StructuredProgramming/threethirtynine.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 
+
 /**
- * (Checkerboard Pattern of Asterisks) Write a program that displays the following checker-board pattern.
+ * main -(Checkerboard Pattern of Asterisks)
+ *
+ * Write a program that
+ * displays the following checker-board pattern.
+ *
+ * Return: 0
  */
-
-int main( void )
+int main(void)
 {
-	// I = Columns.
-	// J = Rows.
-	int i = 1, j = 1;
+        /* I = Columns. */
+        /* J = Rows. */
+        int i = 1, j = 1;
 
-	while (i < 9)
-	{
-		// If is pair print a space extra.
-		if ( i % 2 == 0 )
-			printf( "%s", " " );
+        while (i < 9)
+        {
+                /* If is pair print a space extra. */
+                if (i % 2 == 0)
+                        printf("%s", " ");
 
-		j = 1;
+                j = 1;
 
-		while ( j < 9 )
-		{
-			printf( "%s", "* " );
-			j++;
-		}
-		// New line normal
-		puts( "" );
+                while (j < 9)
+                {
+                        printf("%s", "* ");
+                        j++;
+                }
+                /* New line normal */
+                puts("");
 
-		i++;
-	}
-	return (0);
+                i++;
+        }
+
+        return (0);
 }
