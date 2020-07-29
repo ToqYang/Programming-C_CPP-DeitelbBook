a10663fa399bb7182ee119e634662624e51f61cc
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 12:55:08 -0500

    [Feature] Taxes

--- StructuredProgramming/threeseventeen.c
+++ StructuredProgramming/threeseventeen.c
@@ -1,65 +1,57 @@
 #include <stdio.h>
 
-/** (Sales Tax) Sales tax is collected from buyers and remitted to the government. 
- *  A retailer has to file a monthly sales tax report which lists the sales for
- *  the month and the * amount of sa lestax collected, at both the county and state levels. 
- *  Develop a program that will input the total collections for a month,
- *  calculate the sales tax on the collections, and display the county and statetaxes. 
- *  Assume that states have a 4% sales tax and counties have a 5% sales tax.
- *  Here is a sampleinput/output dialog.
- */
 
 /**
  * main - Print the % discount of taxed
+ * (Sales Tax) Sales tax is collected from buyers and
+ * remitted to the government. A retailer has to file a monthly sales tax
+ * report which lists the sales for the month and the * amount of
+ * sa lestax collected, at both the county and state levels.
+ * Develop a program that will input the total collections for a month,
+ * calculate the sales tax on the collections,
+ * and display the county and statetaxes.
+ *  Assume that states have a 4% sales tax and counties have a 5% sales tax.
+ *  Here is a sampleinput/output dialog.
+ *
  * Return: 0
  */
-
-int main( void )
+int main(void)
 {
-	float t_amm_coll, sales, county, state, total_Tax;
+        float t_amm_coll = 0.0, sales = 0.0, county = 0.0;
+        float state = 0.0, total_Tax = 0.0;
+        char month[10];
+        int c = 0, i = 0;
 
-	char month[10];
+        while (i < 1)
+        {
+                printf("Enter total amount collected (-1 to quit): ");
+                scanf("%f", &t_amm_coll);
 
-	int c, i;
-	c = 0;
-	i = 0;
+                c = t_amm_coll;
 
-	t_amm_coll = 0;
-	sales = 0.0;
-	county = 0.0;
-	state = 0.0;
-	total_Tax = 0.0;
+                if (c == -1)
+                        break;
+                else
+                {
+                        printf("Enter name of month: ");
+                        scanf("%s", month);
 
-	while( i < 1 )
-	{
-		printf( "Enter total amount collected (-1 to quit): " );
-	        scanf( "%f", &t_amm_coll );
+                        printf("Total Collections: $ %.2f\n", t_amm_coll);
 
-		c = t_amm_coll;
+                        county = ((t_amm_coll * 5) / 100);
+                        state = ((t_amm_coll * 4) / 100);
+                        total_Tax = county + state;
 
-		if( c == -1)
-		{
-			break;
-		}
-		else
-		{
-		printf( "Enter name of month: " );
-		scanf( "%s", month );
+                        sales = t_amm_coll - total_Tax;
 
-		printf( "Total Collections: $ %.2f\n", t_amm_coll );
+                        printf("Sales: $ %.2f\n", county);
 
-		county = ( ( t_amm_coll * 5 ) / 100 );
-		state = ( ( t_amm_coll * 4 ) / 100 );
-		total_Tax = county + state;
-
-		sales = t_amm_coll - total_Tax;
-
-		printf( "Sales: $ %.2f\n", county );
-
-		printf( "County Sales Tax: $ %.2f\n", county );
-		printf( "State Sales Tax: $ %.2f\n", state );
-		printf( "Total Sales Tax Collected: $ %.2f\n\n", total_Tax );
-		}
-	}
-	return (0);
+                        printf("County Sales Tax: $ %.2f\n", county);
+                        printf("State Sales Tax: $ %.2f\n", state);
+                        printf("Total Sales Tax Collected: $ %.2f\n\n",
+                               sales);
+                        printf("Earned: %.2f", total_Tax);
+                }
+        }
+        return (0);
 }
