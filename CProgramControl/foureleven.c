19d015d31bd32537c314152a387e40087599b989
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 16:21:10 -0500

    [Feature] Calculate the sum of the multiples

--- CProgramControl/foureleven.c
+++ CProgramControl/foureleven.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 
-/* 
- * (Calculating the Sum of Multiples) Write a program to calculate and print the sum of all
- * multiples of 7 from 1 to 100.
- */
 
-/* 
- * main - Multiples of 7 from 1-100
+/**
+ * main - (Calculating the Sum of Multiples)
+ *
+ * Write a program to
+ * calculate and print the sum of all
+ * multiples of 7 from 1 to 100.
+ *
  * Return: 0 if is success
  */
-
 int main(void)
 {
-	int count;
+        for (int count = 1; count <= 100; count++)
+        {
+                if (count % 7 == 0)
+                {
+                        printf("%d ", count);
+                }
+        }
+        putchar(10);
 
-	count = 1;
-
-	for ( ; count <= 100; count++ )
-	{
-		if ( count % 7 == 0 )
-		{
-			printf("%d ", count);
-		}
-	}
-
-	putchar(10);
-
-	return (0);
+        return (0);
 }
-
