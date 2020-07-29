1407c6ed76c80d9ba519013f12ef22a2020a9e8d
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 15:10:43 -0500

    [Feature] Square asterisks

--- StructuredProgramming/threethirtythree.c
+++ StructuredProgramming/threethirtythree.c
@@ -1,67 +1,61 @@
 #include <stdio.h>
 
+
 /**
- * (Hollow  Square  of  Asterisks)  Modify  the  program  you  wrote  in  Exercise 3.32
+ * main - (Hollow  Square  of  Asterisks)
+ * Modify  the  program  you  wrote  in  Exercise 3.32
  * so  that  itprints a hollow square.
  * For example, if your program reads a size of 5, it should print
+ *
+ * Return: 0 If is success
  */
-
-/**
- * main - Print hollow square
- * Return: 0
- */
-
-int main( void )
+int main(void)
 {
-        int rows, column, size;
+        int rows = 0, column = 0, size = 0;
 
-        rows = 0;
-        column = 0;
-        size = 0;
+        printf("Enter the size: ");
+        scanf("%d", &size);
 
-        printf( "Enter the size: " );
-        scanf( "%d", &size );
-
-        while ( column < size )
+        while (column < size)
         {
 
-		// Print since the first and last line of the column *****
-                if ( column == 0 || ( column == ( size - 1 ) ) )
-		{
-			// Print the rows
-			while ( rows < size )
-			{
-				printf( "*" );
-				rows++;
-			}
-			// Restart of the variable for be, used
-			rows = 0;
-		}
+                /* Print since the first and last line of the column ***** */
+                if (column == 0 || (column == (size - 1)))
+                {
+                        /* Print the rows */
+                        while (rows < size)
+                        {
+                                printf("*");
+                                rows++;
+                        }
+                        /* Restart of the variable for be, used */
+                        rows = 0;
+                }
 
-		// Print after of first line and before the last end line (size - 1)
-		else if ( column >= 1 && ( column < ( size - 1 ) ) )
-		{
-			// Print first *
-			printf( "*" );
+                /* Print after of first line and
+                before the last end line (size - 1) */
+                else if (column >= 1 && (column < (size - 1)))
+                {
+                        /* Print first */
+                        printf("*");
 
-			/**
-			 * rows less than size - 3 for adjust the spaces
-			 * -3 because quit the two characters
-			 * first and last line asterisk
+                        /*
+			 rows less than size - 3 for adjust the spaces
+			 -3 because quit the two characters
+			 first and last line asterisk
 			 */
+                        while (rows <= size - 3)
+                        {
+                                printf(" ");
+                                rows++;
+                        }
 
-			while ( rows <= size - 3 )
-			{
-				printf( " " );
-				rows++;
-			}
+                        /* Print last */
+                        printf("*");
+                        rows = 0;
+                }
 
-			// Print last *
-			printf( "*" );
-			rows = 0;
-		}
-
-                printf( "\n" );
+                putchar(10);
                 column++;
         }
 
