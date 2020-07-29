1114aa7d95d7e7ac8cc727bb70e67ca3b73b93b2
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 16:35:00 -0500

    [Feature] Taxes

--- CProgramControl/fourfourtyone.c
+++ CProgramControl/fourfourtyone.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
 
-/**
- * (Tax Plan Alternatives; The “FairTax”) There are many proposals to make taxation fairer.
- * Check out the FairTax initiative in the United States at www.fairtax.org
- */
 
 /**
  * main - Print the value of the products with taxes
+ *
+ * (Tax Plan Alternatives; The “FairTax”) There are many proposals to make taxation fairer.
+ * Check out the FairTax initiative in the United States at www.fairtax.org
+ *
  * Return: 0 if is success
  */
-
 int main(void)
 {
-	int value = 0, total = 0;
+        int value = 0, total = 0;
 
-	do {
-		puts("\nEnter the value of the product.");
-		scanf("%d", &value);
+        do {
+                puts("\nEnter the value of the product.");
+                scanf("%d", &value);
 
-		total = ( ( value * 27 ) / 100 );
-		
-		printf("\nTotal product is: %d", total);
-		total = 0;
+                total = ((value * 27) / 100);
 
-	} while (value != -1);
+                printf("\nTotal product is: %d", total);
+                total = 0;
 
-	return (0);
+        } while (value != -1);
+
+        return (0);
 }
