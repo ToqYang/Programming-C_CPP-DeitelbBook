75611d03d7428ea0d12d0be9aadc862e4b79d151
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 16:49:40 -0500

    [Feature] Triangle modes print

--- CProgramControl/foursixteen.c
+++ CProgramControl/foursixteen.c
@@ -1,21 +1,23 @@
 #include <stdio.h>
 
-/*
- * (Triangle-Printing Program) Write a program that prints the following patterns separately,
- * one below the other. Use for loops to generate the patterns. All asterisks (*) should be printed by a 
- * single printf statement of the form printf("%s", "*"); (this causes the asterisks to print side by side). 
- *  [Hint: The last two patterns require that each line begin with an appropriate number of blanks.]
- */
 
-/*
- * main - Print different triangles
- * Return: 0 If is success
+/**
+ * main - (Triangle-Printing Program)
+ *
+ * Write a program that prints the following patterns separately,
+ * one below the other. Use for loops to generate the patterns.
+ * All asterisks (*) should be printed by a
+ * single printf statement of the form printf("%s", "*");
+ * (this causes the asterisks to print side by side).
+ *  [Hint: The last two patterns require that
+ * each line begin with an appropriate number of blanks.]
+ *
+ * Return: 0 if is success
  */
-
 int main(void)
 {
-	/* Triangle rectangle */
-/*
+        /* Triangle rectangle */
+        /*
 	for (unsigned int columns = 1; columns <= 10; columns++)
 	{
  		for (unsigned int rows = 1; rows <= columns; rows++)
@@ -27,7 +29,7 @@ int main(void)
 	putchar(10);
 */
 
-/*
+        /*
  * Triangle rectangle invert
 	for (unsigned int columns = 1; columns <= 10; columns++)
 	{
@@ -39,13 +41,13 @@ int main(void)
 	}
 	putchar(10);
 */
-	
-/*
+
+        /*
  * Triangle inverse side and the other wide round
 	for (unsigned int columns = 1; columns <= 10; columns++)
 	{
 		for(unsigned int blank_Spaces = 1; blank_Spaces < columns ; blank_Spaces++)
-		{	
+		{
 			printf(" ");
 		}
 
@@ -58,12 +60,12 @@ int main(void)
 	putchar(10);
 */
 
-/*
+        /*
  * Triangle reverse side
 	for (unsigned int columns = 1; columns <= 10; columns++)
 	{
 		for(unsigned int blank_Spaces = 10; blank_Spaces > columns ; blank_Spaces--)
-		{	
+		{
 			printf(" ");
 		}
 
@@ -75,5 +77,5 @@ int main(void)
 	}
 	putchar(10);
 */
-	return (0);
+        return (0);
 }
