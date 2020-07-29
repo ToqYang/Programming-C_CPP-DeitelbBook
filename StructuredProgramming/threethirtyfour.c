e46ea44557e858a84b697193fc9acf0e38dd8ed4
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 13:20:15 -0500

    [Feature] Floyd triangle

--- StructuredProgramming/threethirtyfour.c
+++ StructuredProgramming/threethirtyfour.c
@@ -1,43 +1,40 @@
 #include <stdio.h>
 
-/**
- * (Floyd's Triangle) Floyd’s Triangle is a right-angled triangular array of natural numbers.
- * It is defined by filling rows with consecutive integers.
- * Thus, row 1 will have the number 1, row 2 willhave the numbers 2 and 3, and so on. 
- * Write a program that draws a 10-line Floyd’s triangle.
- * An outer loop can control the number of lines to be printed and an inner loop can ensure that each rowcontains the correct number of integers.
- */
 
 /**
- * main -Print floyd triangle
+ * main - Print floyd triangle
+ * (Floyd's Triangle) Floyd’s Triangle is a right-angled triangular array
+ * of natural numbers.
+ * It is defined by filling rows with consecutive integers.
+ * Thus, row 1 will have the number 1, row 2 willhave the numbers 2 and 3,
+ * and so on.
+ * Write a program that draws a 10-line Floyd’s triangle.
+ * An outer loop can control the number of lines to be printed and
+ * an inner loop can ensure that each row
+ * contains the correct number of integers.
+ *
  * Return: 0
  */
-
-int main( void )
+int main(void)
 {
-	int column, rows, counter_Nums, counter;
+        int column = 1, rows = 0, counter_Nums = 1, counter = 1;
 
-	column = 1;
-	rows = 0;
-	counter_Nums = 1;
-	counter = 1;
+        printf("Enter the numbers of rows: ");
+        scanf("%d", &rows);
 
-	printf( "Enter the numbers of rows: " );
-	scanf( "%d", &rows );
+        while (counter < rows)
+        {
+                column = 0;
 
-	while ( counter < rows )
-	{
-		column = 0;
+                while (column < counter)
+                {
+                        printf("%d ", counter_Nums);
+                        counter_Nums++;
+                        column++;
+                }
 
-		while ( column < counter )
-		{
-			printf( "%d ", counter_Nums );
-			counter_Nums++;
-			column++;
-		}
-
-		counter++;
-		printf( "\n" );
-	}
-	return (0);
+                counter++;
+                printf("\n");
+        }
+        return (0);
 }
