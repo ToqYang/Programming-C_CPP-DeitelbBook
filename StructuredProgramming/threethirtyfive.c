ecb61b040580247e76906a1fa08a49c355a4644b
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 13:19:57 -0500

    [Feature]  Decimal  to binary

--- StructuredProgramming/threethirtyfive.c
+++ StructuredProgramming/threethirtyfive.c
@@ -1,52 +1,48 @@
 #include <stdio.h>
-#include <math.h>
+
 
 /**
  * main - Printing the Decimal Equivalent of a Binary Number
- * @base: Const base
- * @num_Bynary: Store the number of bynary
- * @i: Counter numbers binary
- * @j: Counter for power
- * @power: Store of power
- * @result: Store the result of binary
+ *
  * Return: 0
  */
-
 int main(void)
 {
-	const base = 2;
+        const int base = 2;
+        int num_Binary = 0, i = 3, j = 1, power = 0, result = 0;
 
-	int num_Binary, i, j, power, result;
+        /*
+        base: Const base
+        num_Bynary: Store the number of bynary
+        i: Counter numbers binary
+        j: Counter for power
+        power: Store of power
+        result: Store the result of binary
+        */
+        while (i >= 0)
+        {
+                power = 0;
 
-	num_Binary = 0;
-	i = 3;
-	j = 1;
-	result = 0;
+                printf("Enter the binary: ");
+                scanf("%d", &num_Binary);
 
-	while ( i >= 0 )
-	{
-		power = 0;
+                while (j <= i)
+                {
+                        power += base * base;
+                        j++;
+                }
 
-		printf( "Enter the binary: " );
-		scanf( "%d", &num_Binary );
+                if (i == 0)
+                {
+                        power++;
+                }
 
-		while ( j <= i )
-		{
-			power += base * base;
-			j++;
-		}
+                result += power;
 
-		if (i == 0)
-		{
-			power++;
-		}
+                i--;
+        }
 
-		result += power;
+        printf("The number binary is: %d\n", result);
 
-		i--;
-	}
-
-	printf( "The number binary is: %d\n", result );
-
-	return (0);
+        return (0);
 }
