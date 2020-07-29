2c06615d225b5b84451055f3fdda135bbc9740a0
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 13:38:44 -0500

    [Feature] Number largest

--- StructuredProgramming/threetwentysix.c
+++ StructuredProgramming/threetwentysix.c
@@ -1,45 +1,40 @@
 #include <stdio.h>
 
-/**
- * (Find the Two Largest Numbers) Using an approach similar to Exercise 3.23,
- * find the twolargest values of the 10 numbers. [Note: You may input each number only once.]
- */
 
 /**
  * main - Print the two numbers more largest
+ * (Find the Two Largest Numbers) Using an approach similar to Exercise 3.23,
+ * find the twolargest values of the 10 numbers.
+ * [Note: You may input each number only once.]
+ *
  * Return: 0
  */
-
-int main( void )
+int main(void)
 {
-        int counter, number, largest, largest_Two;
+        int counter = 0, number = 0, largest = 0, largest_Two = 0;
 
-        counter = 0;
-        number = 0;
-        largest = 0;
-	largest_Two = 0;
-
-        while ( counter < 11 )
+        while (counter < 11)
         {
-                printf( "Number (%d): ", counter );
-                scanf( "%d", &number );
+                printf("Number (%d): ", counter);
+                scanf("%d", &number);
 
-                if ( number >= largest )
+                if (number >= largest)
                 {
                         largest = number;
 
-			if ( counter <= 9 && largest > largest_Two )
-			{
-				largest_Two = largest;
-				printf( "%d-%d", counter, largest_Two );
-			}
-
+                        if (counter <= 9 && largest > largest_Two)
+                        {
+                                largest_Two = largest;
+                                printf("%d-%d", counter, largest_Two);
+                        }
                 }
 
                 counter++;
         }
 
-        printf( "The first number largest is: %d\nThe second number largest is: %d\n", largest, largest_Two );
+        printf("The first number largest is: %d\nThe second number\
+        largest is: %d\n",
+               largest, largest_Two);
 
         return (0);
 }
