6cc8c3edde001e8b01bf8ad4c894e5b816886754
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 11:04:14 -0500

    [Feature] Assign

--- StructuredProgramming/threefour.c
+++ StructuredProgramming/threefour.c
@@ -1,30 +1,26 @@
 #include <stdio.h>
+
+
 /**
  * main - Print different arguments
  * Return: 0
  */
-int main( void )
+int main(void)
 {
-// A) Define variables sum and x to be of type int.
-
+	/* A) Define variables sum and x to be of type int. */
 	int sum, x;
 
-// B) Set variable x to 1.
-
+	/* B) Set variable x to 1. */
 	x = 1;
 
-// C) Set variable sum to 0.
-
+	/* C) Set variable sum to 0. */
 	sum = 0;
 
-// D) Add variable x to variable sum and assign the result to variable sum.
-
+	/* D) Add variable x to variable sum and assign the result to variable sum. */
 	sum += x;
 
-// E) Print "The sum is: " followed by the value of variable sum.
-
+	/* E) Print "The sum is: " followed by the value of variable sum. */
 	printf("The sum is %d\n", sum);
-
 
 	return (0);
 }
