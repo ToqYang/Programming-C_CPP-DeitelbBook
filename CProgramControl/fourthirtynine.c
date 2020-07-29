1922568f21fd216e43da529b31f86f1098f575f3
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 17:59:12 -0500

    [Feature] Limit float

--- CProgramControl/fourthirtynine.c
+++ CProgramControl/fourthirtynine.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
+
 /**
- * (Limitations of Floating-Point Numbers for Monetary Amounts) Section 4.6 cautioned about using floating-point values for monetary calculations. Try this experiment: Create a float
- * variable with the value 1000000.00. Next add to that variable the literal float value 0.12f. Display
+ * main - (Limitations of Floating-Point Numbers for Monetary Amounts)
+ *
+ * Section 4.6 cautioned about using floating-point values
+ * for monetary calculations. Try this experiment: Create a float
+ * variable with the value 1000000.00.
+ *  Next add to that variable the literal float value 0.12f. Display
  * the result using printf and the conversion specifier "%.2f". What do you get?
+ *
+ * Return: 0 if is success
  */
 
 /**
  * main - Look the limitations of float point numbers
  * Return: 0 if is success
  */
-
 int main(void)
 {
-	float monetary = 1000000.00;
-	float result = monetary + 0.12;
+        float monetary = 1000000.00;
+        float result = monetary + 0.12;
 
-	printf("%.2f\n", result);
+        printf("%.2f\n", result);
 
-	return (0);
+        return (0);
 }
