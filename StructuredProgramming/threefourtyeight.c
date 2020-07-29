71928702b13ed2997b21c9295fe3a3dbbc5db3cd
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 11:45:49 -0500

    [Feature] Crypto tasks

--- StructuredProgramming/threefourtyeight.c
+++ StructuredProgramming/threefourtyeight.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 
+
 /**
+ * main - Print number encrypted
+ *
  * (Enforcing Privacy with Cryptography)
  * 1) We add 7 at the number
  * 2) Find the reaminder of the division between 10
@@ -8,105 +11,96 @@
  *
  * For decrypted reverse the scheme and see the module if is 0
  *
- * main - Print number encrypted
  * Return: 0 If, is successful
  */
-
-int main( void )
+int main(void)
 {
-	int num1, num2, num3, num4, number, num_Encrypted, num_Dcry;
+        int num1 = 0, num2 = 0, num3 = 0, num4 = 0, number = 0;
+        int num_Encrypted = 0, num_Dcry = 0;
 
-	num1 = 0;
-	num2 = 0;
-	num3 = 0;
-	num4 = 0;
-	number = 0;
-	num_Encrypted = 0;
-	num_Dcry = 0;
+        /* ENCRYPTED */
 
-	/* ENCRYPTED */
+        printf("Enter the four number: ");
+        scanf("%d", &number);
 
-	printf( "Enter the four number: " );
-	scanf( "%d", &number);
+        /* Take each position number and add +7 */
+        num1 = ((number / 1000) + 7);
+        num2 = (((number / 100) % 10) + 7);
+        num3 = (((number / 10) % 10) + 7);
+        num4 = ((number % 10) + 7);
 
-	/* Take each position number and add +7 */
-	num1 = ( ( number / 1000 ) + 7 );
-	num2 = ( ( ( number / 100 ) % 10 ) + 7 );
-	num3 = ( ( ( number / 10 ) % 10 ) + 7 );
-	num4 = ( ( number % 10 ) + 7 );
+        /* Take out the module to 10 */
+        num1 = num1 % 10;
+        num2 = num2 % 10;
+        num3 = num3 % 10;
+        num4 = num4 % 10;
 
-	/* Take out the module to 10 */
-	num1 = num1 % 10;
-	num2 = num2 % 10;
-	num3 = num3 % 10;
-	num4 = num4 % 10;
+        /* Multiply because, I need add in that order */
 
-	/* Multiply because, I need add in that order */
+        /* Swap positions 3-1 */
+        num3 *= 1000;
+        num1 *= 10;
 
-	/* Swap positions 3-1 */
-	num3 *= 1000;
-	num1 *= 10;
+        /* Swap positions 2-4 */
+        num2 *= 1;
+        num4 *= 100;
 
-	/* Swap positions 2-4 */
-	num2 *= 1;
-	num4 *= 100;
+        /* Sum the values for print the number encrypted */
+        num_Encrypted += num3;
+        num_Encrypted += num4;
+        num_Encrypted += num1;
+        num_Encrypted += num2;
 
-	/* Sum the values for print the number encrypted */
-	num_Encrypted += num3;
-	num_Encrypted += num4;
-	num_Encrypted += num1;
-	num_Encrypted += num2;
+        printf("The number encrypted is: %d\n", num_Encrypted);
 
-	printf( "The number encrypted is: %d\n", num_Encrypted );
+        /* DECRYPTED */
 
-	/* DECRYPTED */
+        /* Get the number other time */
+        num3 = ((num_Encrypted / 1000));
+        num4 = (((num_Encrypted / 100) % 10));
+        num1 = (((num_Encrypted / 10) % 10));
+        num2 = (num_Encrypted % 10);
 
-	/* Get the number other time */
-	num3 = ( ( num_Encrypted / 1000 ) );
-	num4 = ( ( ( num_Encrypted / 100 ) % 10 ) );
-	num1 = ( ( ( num_Encrypted / 10 ) % 10 ) );
-	num2 = ( num_Encrypted % 10 );
+        /* Inverse operation for find the number initial */
+        num3 = ((num3 + 10) - 7);
+        num4 = ((num4 + 10) - 7);
+        num1 = ((num1 + 10) - 7);
+        num2 = ((num2 + 10) - 7);
 
-	/* Inverse operation for find the number initial */
-	num3 = ( ( num3 + 10 ) - 7 );
-	num4 = ( ( num4 + 10 ) - 7 );
-	num1 = ( ( num1 + 10 ) - 7 );
-	num2 = ( ( num2 + 10 ) - 7 );
+        /* If the result if greater than 10 then el module was 0,
+                then decrement in 10 each number */
+        if (num1 >= 10)
+        {
+                num1 -= 10;
+        }
+        if (num2 >= 10)
+        {
+                num2 -= 10;
+        }
+        if (num3 >= 10)
+        {
+                num3 -= 10;
+        }
+        if (num4 >= 10)
+        {
+                num4 -= 10;
+        }
 
+        /* Swap positions 3-1 */
+        num1 *= 1000;
+        num3 *= 10;
 
-	/* If the result if greater than 10 then el module was 0, then decrement in 10 each number */
-	if (num1 >= 10 )
-	{
-		num1 -= 10;
-	}
-	if (num2 >= 10 )
-	{
-		num2 -= 10;
-	}
-	if (num3 >= 10 )
-	{
-		num3 -= 10;
-	}
-	if (num4 >= 10 )
-	{
-		num4 -= 10;
-	}
+        /* Swap positions 4-2 */
+        num4 *= 1;
+        num2 *= 100;
 
-	/* Swap positions 3-1 */
-	num1 *= 1000;
-	num3 *= 10;
+        /* Sum for get only one number */
+        num_Dcry += num1;
+        num_Dcry += num2;
+        num_Dcry += num3;
+        num_Dcry += num4;
 
-	/* Swap positions 4-2 */
-	num4 *= 1;
-	num2 *= 100;
+        printf("\nThe number decrypted is: %d\n", num_Dcry);
 
-	/* Sum for get only one number */
-	num_Dcry += num1;
-	num_Dcry += num2;
-	num_Dcry += num3;
-	num_Dcry += num4;
-
-	printf( "\nThe number decrypted is: %d\n", num_Dcry );
-
-	return (0);
+        return (0);
 }
