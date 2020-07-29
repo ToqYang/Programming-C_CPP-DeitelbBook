735299525d48d63f36dc24e56388be8cc6ae1f7a
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 13:23:55 -0500

    [Feature]  i++ vs ++i

--- StructuredProgramming/threetwentyone.c
+++ StructuredProgramming/threetwentyone.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 
+
 /**
- * (Preincrementing vs Postincrementing) Write a program that demonstrates the difference 
+ * main - (Preincrementing vs Postincrementing)
+ * Write a program that demonstrates the difference
  * between preincrementing and postincrementing using the increment operator ++.
+ *
+ * Return: 0 If is success
  */
-
-// Detect diferences between pre and pos increment.
-
-int main( void )
+int main(void)
 {
-	int pre, pos, x;
-	pre = 0;
-	pos = 0;
-	x = 0;
+        int pre = 0, pos = 0, x = 0;
 
-	while ( pre <= 2 )
-	{
-		x = ++pre;
-		printf( "Preincrement: %d\n", x );
-	}
+        while (pre <= 2)
+        {
+                x = ++pre;
+                printf("Preincrement: %d\n", x);
+        }
 
-	printf( "\n" );
+        putchar(10);
 
-	while ( pos <= 2 )
-	{
-		x = pos++;
-		printf( "Postincrement: %d\n", x );
-	}
+        while (pos <= 2)
+        {
+                x = pos++;
+                printf("Postincrement: %d\n", x);
+        }
 
-	return (0);
-
+        return (0);
 }
