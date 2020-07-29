70ad572faece261b7c1ce7552c2b0e22dd01571c
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 18:08:38 -0500

    [Feature] Roman-Decimal

--- CProgramControl/fourthirtythree.c
+++ CProgramControl/fourthirtythree.c
@@ -1,65 +1,57 @@
 #include <stdio.h>
 
-/**
- * (Roman-Numeral Equivalent of Decimal Values) Write a program that prints a table of all
- * the Roman-numeral equivalents of the decimal numbers in the range 1 to 100.
- */
 
 /**
- * main - Print the roman numeral
+ * main - (Roman-Numeral Equivalent of Decimal Values)
+ *
+ * Write a program that prints a table of all
+ * the Roman-numeral equivalents of the decimal numbers in the range 1 to 100.
+ *
  * Return: 0 if is success
  */
-
 int main(void)
 {
-	char one = 'I', five = 'V', ten = 'X', fifty = 'L', o_hun = 'C';
-	int num = 0;
+        char one = 'I', five = 'V', ten = 'X', fifty = 'L', o_hun = 'C';
+        int num = 0;
 
-	for (int i = 1; i <= 100; i++)
-	{
-		num = i;
+        for (int i = 1; i <= 100; i++) {
+                num = i;
 
-		printf("\n%d: ", num);
+                printf("\n%d: ", num);
 
-		while (num > 0)
-		{
-			if (num == 100)
-			{
-				num -= 100;
-				printf("%c", o_hun);
-				continue;
-			}
+                while (num > 0) {
+                        if (num == 100) {
+                                num -= 100;
+                                printf("%c", o_hun);
+                                continue;
+                        }
 
-			if (num >= 50)
-			{
-				num -= 50;
-				printf("%c", fifty);
-				continue;
-			}
+                        if (num >= 50) {
+                                num -= 50;
+                                printf("%c", fifty);
+                                continue;
+                        }
 
-			if (num >= 10)
-			{
-				num -= 10;
-				printf("%c", ten);
-				continue;
-			}
+                        if (num >= 10) {
+                                num -= 10;
+                                printf("%c", ten);
+                                continue;
+                        }
 
-			if (num >= 5)
-			{
-				num -= 5;
-				printf("%c", five);
-				continue;
-			}
+                        if (num >= 5) {
+                                num -= 5;
+                                printf("%c", five);
+                                continue;
+                        }
 
-			if (num >= 1)
-			{
-				num -= 1;
-				printf("%c", one);
-				continue;
-			}
-		}
-	}
-	putchar(10);
+                        if (num >= 1) {
+                                num -= 1;
+                                printf("%c", one);
+                                continue;
+                        }
+                }
+        }
+        putchar(10);
 
-	return (0);
+        return (0);
 }
