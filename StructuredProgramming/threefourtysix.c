50d33da065b77f741d467def89e6e3418a1901b9
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 12:37:56 -0500

    [Feature] Calculator of World

--- StructuredProgramming/threefourtysix.c
+++ StructuredProgramming/threefourtysix.c
@@ -1,29 +1,25 @@
 #include <stdio.h>
 
+
 /**
- * World-Population-Growth Calculator)
+ * main - (World-Population-Growth Calculator)
  * Print the suposition of 1-5 years the World Population
+ *
  * Return: 0 If, is successful
  */
-
-int main ( void )
+int main(void)
 {
-	int pop_Anual, pop_Total, years, pop_Show;
+        int pop_Anual = 0, pop_Total = 7500, years = 77, pop_Show = 1;
 
-	pop_Show = 0;
-	pop_Total = 7500;
-	pop_Anual = 77;
-	years = 1;
+        while (years <= 5)
+        {
+                pop_Show = 0;
+                pop_Show += (pop_Total + (pop_Anual * years));
 
-	while ( years <= 5 )
-	{
-		pop_Show = 0;
-		pop_Show += ( pop_Total + ( pop_Anual * years) );
+                printf("Year %d: %d\n", years, pop_Show);
 
-		printf( "Year %d: %d\n", years, pop_Show);
+                ++years;
+        }
 
-		++years;
-	}
-
-	return (0);
+        return (0);
 }
