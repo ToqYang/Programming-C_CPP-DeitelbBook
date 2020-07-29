0e553e660b1d9b4513ed37b83232541b0266a64d
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 11:19:26 -0500

    [Feature] Power with root

--- StructuredProgramming/threeseven.c
+++ StructuredProgramming/threeseven.c
@@ -1,39 +1,35 @@
 #include <stdio.h>
 
+
 /**
- * main - Print
+ * main - Power with root
+ *
  * Return: 0
  */
-
-int main ( void )
+int main(void)
 {
 
-/**
- *  Write a C program that uses the statements in Exercise 3.6
- *  to calculate x raised to the y power.
- *  The program should have a while iteration control statement.
- */
-	unsigned int x, y, i, power;
+        /**
+        Write a C program that uses the statements in Exercise 3.6
+        to calculate x raised to the y power.
+        The program should have a while iteration control statement.
+        */
+        unsigned int x = 0, y = 0, i = 1, power = 1;
 
-	x = 0;
-	y = 0;
-	i = 1;
-	power = 1;
+        puts("Enter number base\n");
+        scanf("%u", &x);
 
-	puts("Enter number base\n");
-	scanf("%u", &x);
+        power = x;
 
-	power = x;
+        puts("\nEnter number raised\n");
+        scanf("%u", &y);
 
-	puts("\nEnter number raised\n");
-	scanf("%u", &y);
+        while (i < y)
+        {
+                power *= x;
+                printf("\nThe power is: %u\n", power);
+                i++;
+        }
 
-	while ( i < y )
-	{
-		power *= x;
-		printf("\nThe power is: %u\n", power);
-		i++;
-	}
-
-	return (0);
+        return (0);
 }
