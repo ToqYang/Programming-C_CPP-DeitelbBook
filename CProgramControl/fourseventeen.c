48c5b390543c5c05910c857793c2141e545f9dcb
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 16:44:06 -0500

    [Feature] Credit Limit

--- CProgramControl/fourseventeen.c
+++ CProgramControl/fourseventeen.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
-/*
- * (Calculating Credit Limits) Collecting money becomes increasingly difficult during peri-
- * ods of recession, so companies may tighten their credit limits to prevent their accounts receivable
- * (money owed to them) from becoming too large. In response to a prolonged recession, one company
- * has cut its customers’ credit limits in half. Thus, if a particular customer had a credit limit of $2000,
- * it’s now $1000. If a customer had a credit limit of $5000, it’s now $2500. Write a program that
- * analyzes the credit status of three customers of this company. For each customer you’re given:
+
+/**
+ * main - (Calculating Credit Limits)
+ * Collecting money becomes increasingly
+ * difficult during periods of recession, so companies may tighten
+ * their credit limits to prevent their accounts receivable (money owed to them)
+ * from becoming too large. In response to a prolonged recession, one company
+ * has cut its customers’ credit limits in half.
+ * Thus, if a particular customer had a credit limit of $2000, it’s now $1000.
+ * If a customer had a credit limit of $5000, it’s now $2500.
+ * Write a program that analyzes the credit status
+ * of three customers of this company. For each customer you’re given:
  * a) The customer’s account number.
  * b) The customer’s credit limit before the recession.
- * c) The customer’s current balance (i.e., the amount the customer owes the company).
- * Your program should calculate and print the new credit limit for each customer and should
- * determine (and print) which customers have current balances that exceed their new credit limits
+ * c) The customer’s current balance
+ * (i.e., the amount the customer owes the company).
+ *
+ * Your program should calculate and print
+ * the new credit limit for each customer and should
+ * determine (and print) which customers have current balances that
+ * exceed their new credit limits
+ *
+ * Return: 0 If is success
  */
-
-/* 
- * main - Print the number account, current balance and credi limit
- * Return: 0 if is success
- */
-
 int main(void)
 {
 	int acc_number = 0, cred_lim = 0, bal_curr = 0, cred_rec = 0;
