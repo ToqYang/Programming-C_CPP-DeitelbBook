3ab54e68529e0b3b5ae3c0eb2e1092a1c56b2ef6
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 10:31:25 -0500

    [Feature] Different declarations

--- StructuredProgramming/threethree.c
+++ StructuredProgramming/threethree.c
@@ -1,36 +1,38 @@
 #include <stdio.h>
+
+
 /**
  * main - Print the different declarations
-¨* Return: 0
+ *
+ * Return: 0
  */
-int main( void )
+int main(void)
 {
-// A) Multiply the variable product by 2 using the *= operator
-
+	/* A) Multiply the variable product by 2 using the *= operator */
 	int product;
 
 	product = 5;
 
-//error value  product * 2;
+	/* error value  product * 2; */
 
-// B) Multiply the variable product by 2 using the = and * operators.
+	/* B) Multiply the variable product by 2 using the = and * operators. */
 	product *= 5;
 
-/** C) Test whether the value of the variable count is greater than 10.
-      If it is, print "Count is greater than 10." */
+	/* C) Test whether the value of the variable count is greater than 10.
+      		If it is, print "Count is greater than 10." */
 
 	int count;
 
 	count = 11;
 
-	if ( count > 10 )
+	if (count > 10)
 	{
 		puts("Count is greater than 10.");
 	}
 
-/** D) Calculate the remainder after q is divided by divisor
-       and assign the result to q.
-       Write this statement two different ways. */
+	/* D) Calculate the remainder after q is divided by divisor
+       		and assign the result to q.
+       		Write this statement two different ways. */
 
 	int q;
 
@@ -39,8 +41,8 @@ int main( void )
 	q /= 2;
 	q = q / 2;
 
-/** E) Print the value 123.4567 with two digits of precision.
-       What value is printed? */
+	/* E) Print the value 123.4567 with two digits of precision.
+       		What value is printed? */
 
 	float precision;
 
@@ -48,8 +50,8 @@ int main( void )
 
 	printf("%.2f\n", precision);
 
-/** F) Print the floating-point value 3.14159 with three digits to the right of
-       the decimal point. What value is printed? */
+	/* F) Print the floating-point value 3.14159 with three digits to the right of
+       		the decimal point. What value is printed? */
 
 	float pi;
 
