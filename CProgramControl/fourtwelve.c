57a654ee811b8d5b74448e853f9e40658dff2a1b
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 18:18:06 -0500

    [Feature] Prime 1-100

--- CProgramControl/fourtwelve.c
+++ CProgramControl/fourtwelve.c
@@ -1,44 +1,35 @@
 #include <stdio.h>
 
-/*
- * (Prime Numbers) Write a program to calculate and print a list of all prime numbers * from 1 to 100.
- */
 
-/*
- * main - Print primes of 1 to 100
- * Return:0 if is success
+/**
+ * main - (Prime Numbers)
+ *
+ * Write a program to calculate and
+ * print a list of all prime numbers * from 1 to 100.
+ *
+ * Return: 0 if is success
  */
-
 int main(void)
 {
-	int count, isprime, multiples;
+        /* Lopp to numbers 1 to 100 */
+        for (int count = 2; count <= 100; count++) {
+                /* Loop for know the multiples */
+                int isprime = 1;
+                int multiples = 2;
+                while (multiples <= 100)
+                {
+                        /* Conditional for know if is multiple
+                        and sum if is prime number */
+                        if (count % multiples == 0)
+                                isprime++;
 
-	count = 2;
-	isprime = 1;
-	multiples = 2;
+                        multiples++;
+                }
 
-	/* Lopp to numbers 1 to 100 */
-	for ( ; count <= 100; count++ )
-	{
-		/* Loop for know the multiples */
-		isprime = 1;
-		multiples = 2;
-		while ( multiples <= 100 )
-		{
-			/* Conditional for know if is multiple and sum if is prime number */
-			if (count % multiples == 0 )
-				isprime++;
+                if (isprime < 3)
+                        printf("%d ", count);
+        }
+        putchar(10);
 
-			multiples++;
-		}
-
-		if ( isprime < 3 )
-		{
-			printf("%d ", count);
-		}
-			
-	}
-	putchar(10);
-
-	return (0);
+        return (0);
 }
