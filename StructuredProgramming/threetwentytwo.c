6607fb7c07cf2eca47e3d65b39bca7b3ba0558a0
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 13:46:03 -0500

    [Feature] Prime Number

--- StructuredProgramming/threetwentytwo.c
+++ StructuredProgramming/threetwentytwo.c
@@ -1,45 +1,42 @@
 #include <stdio.h>
 
+
 /**
+ * main - Print number prime
+ *
  * (Checking if a Number is Prime) A prime number is any natural number greater
  * than 1 thatis divisible only by 1 and by itself.
- * Write a C program that reads an integer and determines whetherit is a prime number or not
- */
-
-// main - Print number prime
-// Return: 0
-
-int main( void )
+ * Write a C program that reads an integer and determines
+ * whetherit is a prime number or not
+ *
+ * Return: 0
+*/
+int main(void)
 {
-	int pr_Number, n, count, prime;
+        int pr_Number = 0, n = 0, count = 0;
 
-	prime = 0;
-	count = 0;
-	pr_Number = 0;
+        printf("Enter the prime number: ");
+        scanf("%d", &pr_Number);
 
-	printf( "Enter the prime number: " );
-	scanf( "%d", &pr_Number );
+        if (pr_Number > 1)
+        {
+                for (n = 1; n <= pr_Number; n++)
+                {
+                        if (pr_Number % n == 0)
+                        {
+                                count += 1;
+                        }
+                }
 
-	if ( pr_Number > 1)
-	{
-		for ( n = 1; n <= pr_Number; n++ )
-		{
-			if ( pr_Number % n == 0 )
-			{
-				count += 1;
-			}
-		}
+                if (count == 2)
+                {
+                        printf("%d is a number prime.\n", pr_Number);
+                }
+                else
+                {
+                        printf("%d not is a number prime.\n", pr_Number);
+                }
+        }
 
-		if ( count == 2 )
-		{
-			printf( "%d is a number prime.\n", pr_Number );
-		}
-		else
-		{
-			printf( "%d not is a number prime.\n", pr_Number );
-		}
-
-	}
-
-	return (0);
+        return (0);
 }
