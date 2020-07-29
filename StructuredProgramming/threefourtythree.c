67b6040b829b62f71f9ccfd1cc8da2c0dcb8640b
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 12:44:05 -0500

    [Feature] Side of triangle

--- StructuredProgramming/threefourtythree.c
+++ StructuredProgramming/threefourtythree.c
@@ -1,74 +1,71 @@
 #include <stdio.h>
 
+
 /**
- * (Sides of a Triangle)
  * main - Print if the sides are corrects
+ * (Sides of a Triangle)
+ *
  * Return: 0 If is sucessful
  */
-
-int main( void )
+int main(void)
 {
-	int a, b, c, z;
+        int a = 0, b = 0, c = 0, z = 1;
 
-	a = 0;
-	b = 0;
-	c = 0;
+        puts("Enter the sides of posible triangle.\n");
 
-	puts( "Enter the sides of posible triangle.\n" );
+        while (z != 0)
+        {
+                puts("Enter the a: ");
+                scanf("%d", &z);
 
-	while ( z != 0 )
-	{
-		puts( "Enter the a: " );
-		scanf( "%d", &z );
+                if (z == 0)
+                        break;
 
-		if ( z == 0 )
-			break;
+                a = z;
 
-		a = z;
+                puts("Enter the b: ");
+                scanf("%d", &z);
 
-		puts( "Enter the b: " );
-		scanf( "%d", &z );
+                if (z == 0)
+                        break;
 
-		if ( z == 0 )
-			break;
+                b = z;
 
-		b = z;
+                puts("Enter the c: ");
+                scanf("%d", &z);
 
-		puts( "Enter the c: " );
-		scanf( "%d", &z );
+                if (z == 0)
+                        break;
 
-		if ( z == 0 )
-			break;
+                c = z;
 
-		c = z;
+                if ((a + b) > c)
+                {
+                        puts("The side c is correct");
 
-		if ( ( a + b ) > c )
-		{
-			puts( "The side c is correct" );
+                        if ((a + c) > b)
+                        {
+                                puts("The side b is correct");
+                        }
+                        else
+                        {
+                                puts("The side b not is correct");
+                        }
 
-			if ( ( a + c ) > b )
-			{
-				puts( "The side b is correct" );
-			}
-			else
-			{
-				puts( "The side b not is correct" );
-			}
+                        if ((b + c) > a)
+                        {
+                                puts("The side a is correct");
+                        }
+                        else
+                        {
+                                puts("The side a not is correct");
+                        }
+                }
+                else
+                {
+                        puts("The side c not is correct");
+                }
+        }
 
-			if( ( b + c ) > a )
-			{
-				puts( "The side a is correct" );
-			}
-			else
-			{
-				puts( "The side a not is correct" );
-			}
-		}
-		else
-		{
-			puts( "The side c not is correct" );
-		}
-	}
-
-	return (0);
+        return (0);
 }
