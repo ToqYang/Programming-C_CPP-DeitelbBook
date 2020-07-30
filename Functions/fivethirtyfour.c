054b1eea43b658ade1b7162dbabc077e785ba42e
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 22:08:23 -0500

    [Feature] Power recursion

--- Functions/fivethirtyfour.c
+++ Functions/fivethirtyfour.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
 
+
 int power(int base, int exponent);
 
+
 /**
- * main - 
+ * main - power recursion
+ *
  * Return: 0 if is success
  */
 int main(void)
 {
-	int res = 0;
+        int res = 0;
 
-	res = power(3, 4);
+        res = power(3, 4);
+        printf("The power is: %d\n", res);
 
-	printf("The power is: %d\n", res);
-
-	return (0);
+        return (0);
 }
 
+
+
+/**
+ * power - Recursion functiion power
+ * @base: x
+ * @exponent: y
+ *
+ * Return: Power
+ */
 int power(int base, int exponent)
 {
-	if (exponent <= 0)
-		return (1);
-	return base * power(base, exponent - 1);
+        if (exponent <= 0)
+                return (1);
+        return base * power(base, exponent - 1);
 }
