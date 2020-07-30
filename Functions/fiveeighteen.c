0978a52a0d1e6d4f9145a50e33e0309712bc0bf2
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 19:06:33 -0500

    [Feature] Num even or odd

--- Functions/fiveeighteen.c
+++ Functions/fiveeighteen.c
@@ -1,35 +1,43 @@
 #include <stdio.h>
 
+
 int isEven(int);
 
+
+
 /**
- * (Even or Odd)
- * main - Show if a number is odd, or even
+ * main - (Even or Odd)
+ *
+ * Show if a number is odd, or even
+ *
  * Return: 0 if is success
  */
 int main(void)
 {
-	int number = 0;
+        int number = 0;
 
-	number = isEven(number);
+        number = isEven(number);
 
-	puts("Enter te number.");
-	scanf("%d", &number);
+        puts("Enter te number.");
+        scanf("%d", &number);
 
-	number = isEven(number);
-	
-	printf("(1 is even - 0 is odd): %d\n", number);
+        number = isEven(number);
 
-	return (0);
+        printf("(1 is even - 0 is odd): %d\n", number);
+
+        return (0);
 }
+
 
 /**
  * isEven - Determine if a number is even or odd
+ *
  * Return: 1 if is even and 0 if it isn't
  */
 int isEven(int num)
 {
-	if (num % 2 == 0)
-		return (1);
-	return (0);
+        if (num % 2 == 0)
+                return (1);
+
+        return (0);
 }
