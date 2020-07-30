9db76a92fc881a759d71b7d7769e29d0a8d63552
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 18:49:39 -0500

    [Feature] Operations with matrix

--- Arrays/sixeight.c
+++ Arrays/sixeight.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stddef.h>
+
 #define ROWS 4
 #define COLUMNS 5
+
 
 int largest(int array[][5]);
 int smallest(int array[][5]);
@@ -12,142 +14,158 @@ int sumArray1D(size_t size, int array[size]);
 void modifyArray2D(size_t row, size_t column, int array[row][column], int key);
 void sumDifferences(size_t arr, double array1[arr], double array2[arr]);
 
+
+/**
+ * main - Contain diferent operations with matrix
+ *
+ * Return: (int) 0 if is success
+ */
 int main(void)
 {
-  // a) Multiply the element 4 per 3
-	int n[10] = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20 };
-  int m[3][3];
-  int sales[ROWS][COLUMNS] = {
-    {1, 2, 4, 54},
-    {1, 8, 6, 7},
-    {-1, -5, -15},
-    {50, 40, 30},
-  };
-  int num1[200] = { 1 }, num2[100] = { 0 };
-  double d1[100] = { 100 }, d2[100] = { 50 };
-  
-  // a) Print
-  printArray1D(10, n);
+        /*a) Multiply the element 4 per 3 */
+        int n[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
+        int m[3][3];
+        int sales[ROWS][COLUMNS] = {
+            {1, 2, 4, 54},
+            {1, 8, 6, 7},
+            {-1, -5, -15},
+            {50, 40, 30},
+        };
+        int num1[200] = {1}, num2[100] = {0};
+        double d1[100] = {100}, d2[100] = {50};
 
-  // b) Add all elements
-  printf("The sum of all: %d\n", sumArray1D(10, n));
-	 
-  // c) Initialize array in a two dimensional
-  modifyArray2D(3, 3, m, 3);
-  printArray2D(3, 3, m);
+        /* a) Print */
+        printArray1D(10, n);
 
-  // d) Find smallest and largest inside array
-  printf("The largest number is: %d\n", largest(sales));
-  printf("The smallest number is: %d\n", smallest(sales));
+        /* b) Add all elements */
+        printf("The sum of all: %d\n", sumArray1D(10, n));
 
-  // e) Copy 100 elements
-  puts("-----Original array------\n");
-  printArray1D(200, num1);
-  for (int i = 99; i < 200; i++)
-  {
-    num1[i] = num2[i - 99];
-  }
-  puts("-----Modify array------\n");
-  printArray1D(200, num1);
+        /* c) Initialize array in a two dimensional */
+        modifyArray2D(3, 3, m, 3);
+        printArray2D(3, 3, m);
 
-  // f) Determine the sum and difference
-  sumDifferences(100, d1, d2);
-  
-  return (0);
+        /* d) Find smallest and largest inside array */
+        printf("The largest number is: %d\n", largest(sales));
+        printf("The smallest number is: %d\n", smallest(sales));
+
+        /* e) Copy 100 elements */
+        puts("-----Original array------\n");
+        printArray1D(200, num1);
+        for (int i = 99; i < 200; i++)
+        {
+                num1[i] = num2[i - 99];
+        }
+        puts("-----Modify array------\n");
+        printArray1D(200, num1);
+
+        /* f) Determine the sum and difference */
+        sumDifferences(100, d1, d2);
+
+        return (0);
 }
+
+
 
 /**
  * largest - Find the number largest
- * @array: (int) 
- * Return: The largest number of the array
+ * @array: (int) Contain different Numbers
+ *
+ * Return: (int) The largest number of the array
  */
 int largest(int array[][5])
 {
-  int largest = array[0][0];
+        int largest = array[0][0];
 
-  // Traversing in matrix
-  for (size_t i = 0; i < ROWS; i++)
-  {
-    for (size_t j = 0; j < COLUMNS; j++)
-    {
-      // Compare the first index with the follow
-      // numbers and look if this larger
-      if (largest < array[i][j])
-	largest = array[i][j];
-    }
-  }
+        /* Traversing in matrix */
+        for (size_t i = 0; i < ROWS; i++)
+        {
+                for (size_t j = 0; j < COLUMNS; j++)
+                {
+                        /* Compare the first index with the follow
+                           numbers and look if this larger */
+                        if (largest < array[i][j])
+                                largest = array[i][j];
+                }
+        }
 
-  return (largest);
+        return (largest);
 }
+
 
 /**
  * smallest - Find the number smallest
- * @array: (int) 
- * Return: The smallest number of the array
+ * @array: (int)
+ *
+ * Return: (int) The smallest number of the array
  */
 int smallest(int array[][5])
 {
-  int smallest = array[0][0];
+        int smallest = array[0][0];
 
-  // Traversing in matrix
-  for (size_t i = 0; i < ROWS; i++)
-  {
-    for (size_t j = 0; j < COLUMNS; j++)
-    {
-      // Compare the first index with the follow
-      // numbers and look if this smaller
-      if (smallest > array[i][j])
-	smallest = array[i][j];
-    }
-  }
+        /* Traversing in matrix */
+        for (size_t i = 0; i < ROWS; i++)
+        {
+                for (size_t j = 0; j < COLUMNS; j++)
+                {
+                        /* Compare the first index with the follow
+                           numbers and look if this smaller */
+                        if (smallest > array[i][j])
+                                smallest = array[i][j];
+                }
+        }
 
-  return (smallest);
+        return (smallest);
 }
+
 
 /**
  * printArray1D - Print Scalars
- * @size: Len of the array
+ * @size: (size_t) Len of the array
  * @array: (int) Contiguos variable
  */
 void printArray1D(size_t size, int array[size])
 {
-  for (size_t i = 0; i < size; i++)
-    printf("Array[%lu] = [%d]\n", i, array[i]);
+        for (size_t i = 0; i < size; i++)
+                printf("Array[%lu] = [%d]\n", i, array[i]);
 }
- 
+
+
 /**
  * printArray2D - Print the matrix
- * @size: Len of the array
+ * @row: (size_t) Number of the array
+ * @column: (size_t) Number of the array
  * @array: (int) Contiguos variable
+ *
  */
 void printArray2D(size_t row, size_t column, int array[row][column])
 {
-  for (size_t i = 0; i < row; i++)
-  {
-    for (size_t j = 0; j < column; j++)
-    {
-      printf("Array[%lu][%lu] = %d\n", i, j, array[i][j]);
-    }
-  }
+        for (size_t i = 0; i < row; i++) {
+                for (size_t j = 0; j < column; j++) {
+                        printf("Array[%lu][%lu] = %d\n", i, j, array[i][j]);
+                }
+        }
 }
+
 
 /**
  * sumArray1D - Sum all the element of a array 1D
  * @size: Length of the array
  * @array: Contiguos data to sum
- * Return: Number with the sum of the array
+ *
+ * Return: (int)Number with the sum of the array
  */
 int sumArray1D(size_t size, int array[size])
 {
-  int total = 0;
-  
-  for (size_t i = 0; i < size; i++)
-    total += array[i];
+        int total = 0;
 
-  return (total);
+        for (size_t i = 0; i < size; i++)
+                total += array[i];
+
+        return (total);
 }
 
- /**
+
+/**
  * modifyArray2D - Modify a element of a the matrix
  * @size: Len of the array
  * @array: (int) Contiguos variable
@@ -155,12 +173,12 @@ int sumArray1D(size_t size, int array[size])
  */
 void modifyArray2D(size_t row, size_t column, int array[row][column], int key)
 {
-  for (size_t i = 0; i < row; i++)
-  {
-    for (size_t j = 0; j < column; j++)
-      array[i][j] = key;
-  }
+        for (size_t i = 0; i < row; i++) {
+                for (size_t j = 0; j < column; j++)
+                        array[i][j] = key;
+        }
 }
+
 
 /**
  * sumDifferences - Print the sum and diferences of two arrays
@@ -169,17 +187,17 @@ void modifyArray2D(size_t row, size_t column, int array[row][column], int key)
  * @arr2: Lenght of the second array
  * @array1: (int) Contiguos variable
  * @array2: (int) Contiguos variable
+ *
  */
 void sumDifferences(size_t arr, double array1[arr], double array2[arr])
 {
-  double sum[100] = { 0 }, difference[100] = { 0 };
+        double sum[100] = {0}, difference[100] = {0};
 
-  puts("Sum\t\tDifferences\n");
-  
-  for (size_t i = 0; i < arr; i++)
-  {
-    sum[i] = array1[i] + array2[i];
-    difference[i] = array1[i] - array2[i];
-    printf("%lf\t\t %lf\n", sum[i], difference[i]);
-  }
+        puts("Sum\t\tDifferences\n");
+
+        for (size_t i = 0; i < arr; i++) {
+                sum[i] = array1[i] + array2[i];
+                difference[i] = array1[i] - array2[i];
+                printf("%lf\t\t %lf\n", sum[i], difference[i]);
+        }
 }
