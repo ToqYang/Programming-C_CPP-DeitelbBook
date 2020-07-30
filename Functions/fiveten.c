62fe12ceb583af85147acc906a29046ee2da6df6
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 21:46:03 -0500

    [Feature] Ceil of a function

--- Functions/fiveten.c
+++ Functions/fiveten.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
 #include <math.h>
 
+
 /**
- * (Rounding Numbers)
- * main - Print ceil numbers
+ * main - (Rounding Numbers)
+ *
+ * Print ceil numbers
  * Return: 0 if is success
  */
-
 int main(void)
 {
-	float x = 0, y = 0;
+        float x = 0, y = 0;
 
-	while (x != -1)
-	{ 
-		x = 0;
-		puts("\nEnter the number.");
-		scanf("%f", &x);
+        while (x != -1)
+        {
+                x = 0;
+                puts("\nEnter the number.");
+                scanf("%f", &x);
 
-		/* Round numbers */
-		y = ceil(x + .5);
+                /* Round numbers */
+                y = ceil(x + .5);
 
-		printf("Original number: %.2f\n", x);
-		printf("Number: %.2f\n", y);
-	 }
+                printf("Original number: %.2f\n", x);
+                printf("Number: %.2f\n", y);
+        }
 
-	return (0);
+        return (0);
 }
