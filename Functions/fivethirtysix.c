926dfcfde837ac5ab9546af9ab1d0bb4b1f39927
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 11:21:40 -0500

    [Feature] Towers of Hanoi in recursion

--- Functions/fivethirtysix.c
+++ Functions/fivethirtysix.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
+
 void towerHanoi(int n, int peg_Ini, int peg_moved, int peg_tmp);
+
 
 /**
  * main - Enter to resolve the game Towers of Hanoi
@@ -8,16 +10,18 @@ void towerHanoi(int n, int peg_Ini, int peg_moved, int peg_tmp);
  */
 int main(void)
 {
-	/* Disks: 4
-	 * Towers:
-	 * 1: Initial
-	 * 2: Destine
-	 * 3: Temporal
+        /* Disks: 4
+	   Towers:
+	   1: Initial
+	   2: Destine
+	   3: Temporal
 	 */
-	towerHanoi(3, 1, 3, 2);
+        towerHanoi(3, 1, 3, 2);
 
-	return (0);
+        return (0);
 }
+
+
 
 /**
  * towerHanoi - Print the resolution through recursion of the problem
@@ -26,22 +30,23 @@ int main(void)
  * @peg_Ini: Tower initially where is the disks
  * @peg_Moved: Tower to leave the disks
  * @peg_Tmp: Tower temporal to move the disks
+ *
  */
 void towerHanoi(int n, int peg_Ini, int peg_Moved, int peg_Tmp)
 {
-	/**
-	 * When the disks == 1
-	 * Move initial to last position
+        /*
+	  When the disks == 1
+	  Move initial to last position
 	 */
-	if (n == 1)
-	{
-		printf("%d -> %d\n", peg_Ini, peg_Moved);
-		return;
-	}
-	/* Use second position how temporal to carry to the destine */
-	towerHanoi(n - 1, peg_Ini, peg_Tmp, peg_Moved);
-	printf("%d -> %d\n", peg_Ini, peg_Moved);
+        if (n == 1)
+        {
+                printf("%d -> %d\n", peg_Ini, peg_Moved);
+                return;
+        }
+        /* Use second position how temporal to carry to the destine */
+        towerHanoi(n - 1, peg_Ini, peg_Tmp, peg_Moved);
+        printf("%d -> %d\n", peg_Ini, peg_Moved);
 
-	/* Use posiion initial how tmp */
-	towerHanoi(n - 1, peg_Tmp, peg_Moved, peg_Ini);
+        /* Use posiion initial how tmp */
+        towerHanoi(n - 1, peg_Tmp, peg_Moved, peg_Ini);
 }
