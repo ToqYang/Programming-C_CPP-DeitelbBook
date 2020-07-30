84ee5799627fe0822ac9a6a7644c51bcb3cbe006
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 20:24:54 -0500

    [Feature] Multiply recursion

--- Functions/fivefourtyfour.c
+++ Functions/fivefourtyfour.c
@@ -1,37 +1,44 @@
 #include <stdio.h>
 
-int mystery(int a, int b); // function prototype
+
+/* function prototype */
+int mystery(int a, int b);
+
 
 /**
  * main - Take two numbers and thegive at the function
+ *
  * Return: 0 If is success
  */
 int main(void)
 {
 	printf("%s", "Enter two positive integers: ");
-	// Delete unsigned 
-	int x; // first integer
-	int y; // second integer
-	scanf("%u%u", &x, &y);
+	/* Delete unsigned */
+        /* first integer and second integer */
+	int x;
+        int y;
+        scanf("%u%u", &x, &y);
 
-	printf("The result is %u\n", mystery(x, y));
+        printf("The result is %u\n", mystery(x, y));
 
 	return (0);
-} 
+}
+
+
 
 /**
  * mystery - Multiply the number a to b
- * @a: (int) Number base  
+ * @a: (int) Number base
  * @b: (int) Number of time to sum the num a itself
  * Return: Result of the multiplication
  */
 int mystery(int a, int b)
-{ 
+{
  	// base case
 	if (1 == b) {
 		return a;
-	} 
+	}
 	else { // recursive step
 		return a + mystery(a, b - 1);
-	} 
+	}
 }
