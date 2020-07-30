eed2e4e72017a0a9a14db521105a586797af3f09
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 11:41:39 -0500

    [Feature] GCD

--- Functions/fivetwentynine.c
+++ Functions/fivetwentynine.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 
+
 int gcd(int x, int y);
+
 
 /**
  * main - Find the maximum common divsor
- * Return: 0 if is success
+ *
+ * Return: (int) 0 if is success
  */
 int main(void)
 {
-	printf("Divisor of (6)(4) is:%d\n", gcd(6, 4));
-	printf("Divisor of (60)(72) is:%d\n", gcd(60, 72));
+        printf("Divisor of (6)(4) is:%d\n", gcd(6, 4));
+        printf("Divisor of (60)(72) is:%d\n", gcd(60, 72));
 
-	return (0);
+        return (0);
 }
 
+
 /**
- * gcd - (Recursive Greatest Common Divisor)  
+ * gcd - (Recursive Greatest Common Divisor)
  * @x: (int) Store the first number
  * @y: (int) Store the second number
- * Return: Common divisor
+ *
+ * Return: (int) Common divisor
  */
 int gcd(int x, int y)
 {
-	if (y == 0)
-		return (x);
-	return gcd(y, x % y);
+        if (y == 0)
+                return (x);
+
+        return gcd(y, x % y);
 }
