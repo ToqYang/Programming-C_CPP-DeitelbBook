254d64cf9d50cfafa8e24dac859221b57a1b1409
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-29 19:16:19 -0500

    [Feature] Rounded Numbers

--- Functions/fiveeleven.c
+++ Functions/fiveeleven.c
@@ -1,99 +1,107 @@
 #include <stdio.h>
 #include <math.h>
 
+
 float roundToInteger(float number);
 float roundToTenths(float number);
 float roundToHundreths(float number);
 float roundToThousandths(float number);
 
+
 /**
- * (Rounding Numbers)
- * main - Call functions that rounding numbers
+ * main - (Rounding Numbers)
+ *
+ * Call functions that rounding numbers
+ *
  * Return: 0 if is success
  */
 int main(void)
 {
-	float x = 0, y = 0;
+        float x = 0, y = 0;
 
-	x = 1.4;
-	y = roundToInteger(x);
-        printf("Original number: %f\n", x);
-        printf("Number: %lf\n", y);
-        
-	
-	x = 10.4;
-	y = roundToInteger(x);
+        x = 1.4;
+        y = roundToInteger(x);
         printf("Original number: %f\n", x);
         printf("Number: %lf\n", y);
 
-	
-	x = 100.4;
-	y = roundToHundreths(x);
+        x = 10.4;
+        y = roundToInteger(x);
         printf("Original number: %f\n", x);
         printf("Number: %lf\n", y);
 
-	
-	x = 1000.4;
-	y = roundToThousandths(x);
+        x = 100.4;
+        y = roundToHundreths(x);
         printf("Original number: %f\n", x);
         printf("Number: %lf\n", y);
 
-	return (0);
+        x = 1000.4;
+        y = roundToThousandths(x);
+        printf("Original number: %f\n", x);
+        printf("Number: %lf\n", y);
+
+        return (0);
 }
-
 
 /**
  * roundToInteger - Round a number
- * Return: Number, already it rounded
+ * @number: (float) Number to receive
+ *
+ * Return: (float) Number rounded
  */
 float roundToInteger(float number)
 {
-	float x = number, y = 0;
+        float x = number, y = 0;
 
-	/* Rounding the number until the floor */
-	y = floor(x + .5);
+        /* Rounding the number until the floor */
+        y = floor(x + .5);
 
-	return (y);
+        return (y);
 }
 
 /**
  * roundToTenths - Round a number in tens
- * Return: Number, already it rounded
+ * @number: (float) Number to receive
+ *
+ * Return: N(float) Number, already it rounded
  */
 float roundToTenths(float number)
 {
-	float x = number, y = 0;
+        float x = number, y = 0;
 
-	/* Rounding the number until the floor */
-	y = floor(x + .5);
+        /* Rounding the number until the floor */
+        y = floor(x + .5);
 
-	return (y);
+        return (y);
 }
 
 /**
  * roundToHundreths - Round a number hundreths
- * Return: Number, already it rounded
+ * @number: (float) Number to receive
+ *
+ * Return: (float) Number, already it rounded
  */
 float roundToHundreths(float number)
 {
-	float x = number, y = 0;
+        float x = number, y = 0;
 
-	/* Rounding the number until the floor */
-	y = floor(x + .5);
+        /* Rounding the number until the floor */
+        y = floor(x + .5);
 
-	return (y);
+        return (y);
 }
-	
+
 /**
  * roundToThousandths - Round a number thousandths
- * Return: Number, already it rounded
+ * @number: (float) Number to receive
+ *
+ * Return: (float) Number, already it rounded
  */
 float roundToThousandths(float number)
 {
-	float x = number, y = 0;
+        float x = number, y = 0;
 
-	/* Rounding the number until the floor */
-	y = floor(x + .5);
+        /* Rounding the number until the floor */
+        y = floor(x + .5);
 
-	return (y);
+        return (y);
 }
