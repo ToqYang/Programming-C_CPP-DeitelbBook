59d4669ce975fdeaea06b14339e3d006b26a05a9
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 11:39:27 -0500

    [Feature] Conversión money

--- Functions/fivetwentyfour.c
+++ Functions/fivetwentyfour.c
@@ -1,81 +1,85 @@
 #include <stdio.h>
 
+
 int toYen(int usd);
 int toEuro(int usd);
 void print(int valYen, int valEur);
 
 
 /**
- * (Currency Conversion) 
  *
- * main - Foreign exchanges
- * Return: 0 if is success
+ * main - (Currency Conversion) Foreign exchanges
+ *
+ * Return: (int) 0 if is success
  */
 int main(void)
 {
-	int usd = 0, eur = 0, yen = 0;
+        int usd = 0, eur = 0, yen = 0;
 
-	puts("Enter your number of USD.");
-	scanf("%d", &usd);
+        puts("Enter your number of USD.");
+        scanf("%d", &usd);
 
-	yen = toYen(usd);
-	eur = toEuro(usd);
-	print(yen, eur);
+        yen = toYen(usd);
+        eur = toEuro(usd);
+        print(yen, eur);
 
-	return (0);
+        return (0);
 }
+
 
 
 /**
  * toYen - Calculate the equivalence of the USD in Yen
+ * @usd: (int) Dollars Received
  *
- * @usd: Dollars Received
- * Return: The value of Dollars in Yen
+ * Return: (int) The value of Dollars in Yen
  */
 int toYen(int usd)
 {
-	return (usd * 118.87);
+        return (usd * 118.87);
 }
+
 
 /**
  * toEuro - Calculate the equivalence of the USD in Euros
+ * @usd: (int) Dollars Received
  *
- * @usd: Dollars Received
- * Return: The value of Dollars in Euro
+ * Return: (int) The value of Dollars in Euro
  */
 int toEuro(int usd)
 {
-	return (usd * 0.92);
+        return (usd * 0.92);
 }
+
 
 /**
  * print - Calculate the printing of Yen and Euro
- *
  * @valYen: (int) Value of the Yen
  * @valEur: (int) Value of the Euro
+ *
  */
 void print(int valYen, int valEur)
 {
-	puts("-------------------------");
-	printf("Yens: %d", valYen);
+        puts("-------------------------");
+        printf("Yens: %d", valYen);
 
-	for (int i = 0; i <= valYen; ++i)
-	{
-		if (i % 30 == 0)
-			putchar(10);
-		else
-			putchar(42);
-	}
+        for (int i = 0; i <= valYen; ++i)
+        {
+                if (i % 30 == 0)
+                        putchar(10);
+                else
+                        putchar(42);
+        }
 
-	puts("\n-------------------------");
-	printf("Euros: %d", valEur);
+        puts("\n-------------------------");
+        printf("Euros: %d", valEur);
 
-	for (int j = 0; j <= valEur; ++j)
-	{
-		if (j % 10 == 0)
-			putchar(10);
-		else
-			putchar(42);
-	}
-	putchar(10);
+        for (int j = 0; j <= valEur; ++j)
+        {
+                if (j % 10 == 0)
+                        putchar(10);
+                else
+                        putchar(42);
+        }
+        putchar(10);
 }
