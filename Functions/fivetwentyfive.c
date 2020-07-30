3d8a22fc54c1f4253ce2da38d481e86271e48806
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 11:34:58 -0500

    [Feature] Is posible compare 4 floats

--- Functions/fivetwentyfive.c
+++ Functions/fivetwentyfive.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
 
+
 float findMaximum(float a, float b, float c, float d);
+
 
 /**
  * main - (Find the Maximum)
- * Return: 0 If is success
+ *
+ * Return: (int) 0 If is success
  */
 int main(void)
 {
-	float num1 = 0.007, num2 = 1.6, num3 =0.009, num4 = 0.00007;	
-	num1 = findMaximum(num1, num2, num3, num4);
+        float num1 = 0.007, num2 = 1.6, num3 = 0.009, num4 = 0.00007;
+        num1 = findMaximum(num1, num2, num3, num4);
 
-	printf("The num greatest is: %f\n", num1);
+        printf("The num greatest is: %f\n", num1);
 
-	return (0);
+        return (0);
 }
+
+
 
 /**
  * findMaximum - Compare 4 floats and find the greatest
@@ -22,18 +27,19 @@ int main(void)
  * @b: Second float
  * @c: Third float
  * @d: Four float
- * Return: Greatest float
+ *
+ * Return: (float) Greatest float
  */
 float findMaximum(float a, float b, float c, float d)
 {
-	float precision = 0.00001, greatest = a;
+        float precision = 0.00001, greatest = a;
 
-	if ((greatest - b) < precision)
-		greatest = b;
-	if ((greatest - c) < precision)
-		greatest = c;
-	if ((greatest - d) < precision)
-		greatest = d;
+        if ((greatest - b) < precision)
+                greatest = b;
+        if ((greatest - c) < precision)
+                greatest = c;
+        if ((greatest - d) < precision)
+                greatest = d;
 
-	return (greatest);
+        return (greatest);
 }
