de4f80751d7ee692b0d2a39402bf2922b810edd9
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 11:53:04 -0500

    [Feature] Perfect Num

--- Functions/fivetwentysix.c
+++ Functions/fivetwentysix.c
@@ -1,45 +1,49 @@
 #include <stdio.h>
 
+
 void isPerfect(int a);
 
+
 /**
- * main - Entry point of the program
- * (Perfect Numbers) 
- * Return: 0 if is success
+ * main -  (Perfect Numbers) Entry point of the program
+ *
+ * Return: (int) 0 if is success
  */
 int main(void)
 {
-	int num = 8128, num2 = 23;
+        int num = 8128, num2 = 23;
 
-	isPerfect(num);
-	isPerfect(num2);
+        isPerfect(num);
+        isPerfect(num2);
 
-	return (0);
+        return (0);
 }
+
+
 
 /**
  * isPerfect - Find if a number is perfect
  * @a: Number to be check
+ *
  */
 void isPerfect(int a)
 {
-	int count = 1, res = 0;
+        int count = 1, res = 0;
 
-	/* Find the divisors an the sum */
-	while (count < a)
-	{
-		if (a % count == 0)
-			res += count;
+        /* Find the divisors an the sum */
+        while (count < a)
+        {
+                if (a % count == 0)
+                        res += count;
 
-		++count;
-	}
+                ++count;
+        }
 
-	/* Verify if the sum of the divisors
-	 * is equal to initial num
+        /* Verify if the sum of the divisors
+	   is equal to initial num
 	 */
-	if (res == a)
-		printf("The %d is a number perfect\n", a);
-	else
-		printf("The %d not is a perfect number\n", a);
-	
+        if (res == a)
+                printf("The %d is a number perfect\n", a);
+        else
+                printf("The %d not is a perfect number\n", a);
 }
