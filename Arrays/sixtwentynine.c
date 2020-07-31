bc580886d964b74069e29829a54a7b1b0f099d4e
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 20:02:45 -0500

    [Feature] Knight Tour: Close Tour

--- Arrays/sixtwentynine.c
+++ Arrays/sixtwentynine.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+
+
 #define COLUMNS 8
 #define ROWS 8
 #define MOVEMENTS 8
+
 
 int sanitize_num(int num, int maximum);
 int possible_move(int newx, int newy);
@@ -12,106 +15,106 @@ void closed_tour(int map[ROWS][COLUMNS], int hori[MOVEMENTS], int vert[MOVEMENTS
 
 /**
  * main - (Knight’s Tour: Closed Tour Test)
+ *
  *        Verify if it give a closed tour
  *
  * Return: 0 if is success
  */
 int main(void)
 {
-	int board[ROWS][COLUMNS] = {0}, steps[ROWS][COLUMNS] = {0};
-	int horizontal[MOVEMENTS] = {2, 1, -1, -2, -2, -1, 1, 2};
-	int vertical[MOVEMENTS] = {-1, -2, -2, -1, 1, 2, 2, 1};
+        int board[ROWS][COLUMNS] = {0}, steps[ROWS][COLUMNS] = {0};
+        int horizontal[MOVEMENTS] = {2, 1, -1, -2, -2, -1, 1, 2};
+        int vertical[MOVEMENTS] = {-1, -2, -2, -1, 1, 2, 2, 1};
 
-	int posx[MOVEMENTS] = {0}, posy[MOVEMENTS] = {0};
-	int movenum = 0, currrow = 3, currcol = 4;
-	int countmovements = 1, countpos = 0;
-	int accessibility[ROWS][COLUMNS] = { {2, 3, 4, 4, 4, 4, 3, 2},
-					     {3, 4, 6, 6, 6, 6, 4, 3},
-					     {4, 6, 8, 8, 8, 8, 6, 4},
-					     {4, 6, 8, 8, 8, 8, 6, 4},
-					     {4, 6, 8, 8, 8, 8, 6, 4},
-					     {4, 6, 8, 8, 8, 8, 6, 4},
-					     {3, 4, 6, 6, 6, 6, 4, 3},
-					     {2, 3, 4, 4, 4, 4, 3, 2}
-	};
-	int posheuristic = 0, min = 0, useheu = 0, k = 0;
-	int correctpos[MOVEMENTS] = {0};
+        int posx[MOVEMENTS] = {0}, posy[MOVEMENTS] = {0};
+        int movenum = 0, currrow = 3, currcol = 4;
+        int countmovements = 1, countpos = 0;
+        int accessibility[ROWS][COLUMNS] = {{2, 3, 4, 4, 4, 4, 3, 2},
+                                            {3, 4, 6, 6, 6, 6, 4, 3},
+                                            {4, 6, 8, 8, 8, 8, 6, 4},
+                                            {4, 6, 8, 8, 8, 8, 6, 4},
+                                            {4, 6, 8, 8, 8, 8, 6, 4},
+                                            {4, 6, 8, 8, 8, 8, 6, 4},
+                                            {3, 4, 6, 6, 6, 6, 4, 3},
+                                            {2, 3, 4, 4, 4, 4, 3, 2}};
+        int posheuristic = 0, min = 0, useheu = 0, k = 0;
+        int correctpos[MOVEMENTS] = {0};
 
-	board[currrow][currcol] = 65;
+        board[currrow][currcol] = 65;
 
-	/* Optional: 64 tours possible */
-	for (; countmovements <= 64; ++countmovements) {
-		min = accessibility[currcol][currrow];
-		for (int i = 0; i < MOVEMENTS; ++i) {
-			posx[i] = currcol + horizontal[i];
-			posy[i] = currrow + vertical[i];
-			if (possible_move(posx[i], posy[i]) == 1) {
-				if (countpos == 0) {
-					board[posy[i]][posx[i]] = 70;
-					correctpos[k] = 0;
-					++k;
-				} else {
-					board[posy[i]][posx[i]] = countpos;
-					correctpos[k] = countpos;
-					++k;
-				}
-				if (accessibility[posy[i]][posx[i]] <= min && steps[posy[i]][posx[i]] == 0) {
-					min = accessibility[posy[i]][posx[i]];
-					posheuristic = countpos;
-					++useheu;
-				}
-			}
-			++countpos;
+        /* Optional: 64 tours possible */
+        for (; countmovements <= 64; ++countmovements) {
+                min = accessibility[currcol][currrow];
+                for (int i = 0; i < MOVEMENTS; ++i) {
+                        posx[i] = currcol + horizontal[i];
+                        posy[i] = currrow + vertical[i];
+                        if (possible_move(posx[i], posy[i]) == 1) {
+                                if (countpos == 0) {
+                                        board[posy[i]][posx[i]] = 70;
+                                        correctpos[k] = 0;
+                                        ++k;
+                                } else {
+                                        board[posy[i]][posx[i]] = countpos;
+                                        correctpos[k] = countpos;
+                                        ++k;
+                                }
+                                if (accessibility[posy[i]][posx[i]] <= min && steps[posy[i]][posx[i]] == 0)
+                                {
+                                        min = accessibility[posy[i]][posx[i]];
+                                        posheuristic = countpos;
+                                        ++useheu;
+                                }
+                        }
+                        ++countpos;
+                }
 
-		}
+                if (useheu == 0)
+                        posheuristic = correctpos[useheu];
 
-		if (useheu == 0)
-			posheuristic = correctpos[useheu];
+                while (steps[posy[posheuristic]][posx[posheuristic]] != 0 && useheu < k) {
+                        posheuristic = correctpos[useheu];
+                        ++useheu;
+                        if (possible_move(posx[posheuristic], posy[posheuristic]) == 0) {
+                                break;
+                                countmovements = 64;
+                        }
+                }
 
-		while (steps[posy[posheuristic]][posx[posheuristic]] != 0 && useheu < k) {
-			posheuristic = correctpos[useheu];
-			++useheu;
-			if (possible_move(posx[posheuristic], posy[posheuristic]) == 0) {
-			    break;
-			    countmovements = 64;
-			}
-		}
+                movenum = posheuristic;
 
+                /* Introduce the Number of Step */
+                puts("\nSteps to the ChessBoard\n");
+                steps[currrow][currcol] = countmovements;
+                print_2d(steps);
 
-		movenum = posheuristic;
+                printf("\nMovements: %d\n", countmovements);
+                print_2d(board);
 
-		/* Introduce the Number of Step */
-		puts("\nSteps to the ChessBoard\n");
-		steps[currrow][currcol] = countmovements;
-		print_2d(steps);
+                print_pos(posx, posy);
 
-		printf("\nMovements: %d\n", countmovements);
-		print_2d(board);
+                /* Clear Screen */
+                for (int i = 0; i < ROWS; ++i) {
+                        for (int j = 0; j < COLUMNS; ++j) {
+                                board[i][j] = 0;
+                        }
+                }
 
-		print_pos(posx, posy);
+                currcol += horizontal[movenum];
+                currrow += vertical[movenum];
 
-		/* Clear Screen */
-		for (int i = 0; i < ROWS; ++i) {
-			for (int j = 0; j < COLUMNS; ++j) {
-				board[i][j] = 0;
-			}
-		}
+                board[currrow][currcol] = 65;
+                countpos = 0;
 
-		currcol += horizontal[movenum];
-		currrow += vertical[movenum];
+                posheuristic = 0;
+                min = 0;
+                useheu = 0;
+                k = 0;
+        }
+        closed_tour(steps, horizontal, vertical);
 
-		board[currrow][currcol] = 65;
-		countpos = 0;
-
-		posheuristic = 0;
-		min = 0;
-		useheu = 0;
-		k = 0;
-	}
-	closed_tour(steps, horizontal, vertical);
-
-	return (0);
+        return (0);
 }
+
 
 
 /**
@@ -119,11 +122,11 @@ int main(void)
  * @num: (int) Copy of the number to look if is correct
  * @maximum: (int) Size of the maximum of values
  *
- * Return: 1 if the number is correct, otherwise 0
+ * Return: (int) 1 if the number is correct, otherwise 0
  */
 int sanitize_num(int num, int maximum)
 {
-	return ((num <= maximum && num >= 0) ? 1 : 0);
+        return ((num <= maximum && num >= 0) ? 1 : 0);
 }
 
 
@@ -132,37 +135,44 @@ int sanitize_num(int num, int maximum)
  * @newx: (int) Number of the possible position x
  * @newy: (int) Number of the possible position y
  *
- * Return: 1 if the movement is posible, otherwise 0
+ * Return: (int) 1 if the movement is posible, otherwise 0
  */
 int possible_move(int newx, int newy)
 {
-	return(((newx >= 0 && newx <= 7) && (newy >= 0 && newy <= 7)) ? 1 : 0);
+        return (((newx >= 0 && newx <= 7) && (newy >= 0 && newy <= 7)) ? 1 : 0);
 }
 
 
 /**
  * print_2d - Print the chessboard
- * @map: (int) Matrix to prin
+ * @map: (int) Matrix to print
+ *
  */
 void print_2d(int map[ROWS][COLUMNS])
 {
-	printf("    0  1  2  3  4  5  6  7\n");
-	for (size_t i = 0; i < ROWS; ++i) {
-		printf("%ld ", i);
-		for (size_t j = 0; j < COLUMNS; ++j) {
-			if (map[i][j] == 0) {
-				printf("%3s", "-");
-			}
-			if (map[i][j] == 65) {
-				printf("%3s", "K");
-			} else if (map[i][j] >= 1 && map[i][j] <= 64) {
-				printf(" %2d", map[i][j]);
-			}
-			if (map[i][j] == 70)
-				printf("%3s", "0");
-		}
-		putchar(10);
-	}
+        printf("    0  1  2  3  4  5  6  7\n");
+        for (size_t i = 0; i < ROWS; ++i)
+        {
+                printf("%ld ", i);
+                for (size_t j = 0; j < COLUMNS; ++j)
+                {
+                        if (map[i][j] == 0)
+                        {
+                                printf("%3s", "-");
+                        }
+                        if (map[i][j] == 65)
+                        {
+                                printf("%3s", "K");
+                        }
+                        else if (map[i][j] >= 1 && map[i][j] <= 64)
+                        {
+                                printf(" %2d", map[i][j]);
+                        }
+                        if (map[i][j] == 70)
+                                printf("%3s", "0");
+                }
+                putchar(10);
+        }
 }
 
 
@@ -174,10 +184,11 @@ void print_2d(int map[ROWS][COLUMNS])
  */
 void print_pos(int x[MOVEMENTS], int y[MOVEMENTS])
 {
-	for (size_t i = 0; i < MOVEMENTS; ++i) {
-		printf("Horizonatal[%ld] = %d\tVertical[%ld] = %d\n",
-		       i, x[i], i, y[i]);
-	}
+        for (size_t i = 0; i < MOVEMENTS; ++i)
+        {
+                printf("Horizonatal[%ld] = %d\tVertical[%ld] = %d\n",
+                       i, x[i], i, y[i]);
+        }
 }
 
 
@@ -189,30 +200,32 @@ void print_pos(int x[MOVEMENTS], int y[MOVEMENTS])
  * @map: (int) Contain the map of steps of the chessboard
  * @hori: (int) Move predefined in X
  * @vert: (int) Move predefined in Y
+ *
  */
 void closed_tour(int map[ROWS][COLUMNS], int hori[MOVEMENTS], int vert[MOVEMENTS])
 {
-	int x = 0, y = 0, posx = 0, posy = 0;
+        int x = 0, y = 0, posx = 0, posy = 0;
 
-	for (int i = 0; i < MOVEMENTS; ++i) {
-		for (int j = 0; j < MOVEMENTS; ++j) {
-			if (map[i][j] == 1) {
-				x = j;
-				y = i;
-			}
-		}
-	}
+        for (int i = 0; i < MOVEMENTS; ++i) {
+                for (int j = 0; j < MOVEMENTS; ++j) {
+                        if (map[i][j] == 1)
+                        {
+                                x = j;
+                                y = i;
+                        }
+                }
+        }
 
-	for (size_t k = 0; k < MOVEMENTS; ++k) {
-		posx = x + hori[k];
-		posy = y + vert[k];
+        for (size_t k = 0; k < MOVEMENTS; ++k) {
+                posx = x + hori[k];
+                posy = y + vert[k];
 
-		if (map[posy][posx] == 64) {
-			puts("There a Closed Tour");
-			x = 1;
-			k = 10;
-		}
-	}
-	if (x != 1)
-		puts("No there Closed Tour");
+                if (map[posy][posx] == 64) {
+                        puts("There a Closed Tour");
+                        x = 1;
+                        k = 10;
+                }
+        }
+        if (x != 1)
+                puts("No there Closed Tour");
 }
