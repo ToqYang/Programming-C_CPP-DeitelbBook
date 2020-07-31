b3ba82b1c2972bfcc2da376210d34b5d878cb4ea
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 19:52:35 -0500

    [Feature] Knight Tour Brute Force

--- Arrays/sixtwentyfive.c
+++ Arrays/sixtwentyfive.c
@@ -2,9 +2,11 @@
 #include <time.h>
 #include <stdlib.h>
 
+
 #define COLUMNS 8
 #define ROWS 8
 #define MOVEMENTS 8
+
 
 int sanitize_num(int num, int maximum);
 int possible_move(int newx, int newy);
@@ -13,6 +15,7 @@ void print_pos(int x[MOVEMENTS], int y[MOVEMENTS]);
 int posible_positions(int pos[MOVEMENTS], int size);
 void full_tour(int his[MOVEMENTS]);
 
+
 /**
  * main - (Knight’s Tour: Brute-Force Approaches)
  *
@@ -20,100 +23,102 @@ void full_tour(int his[MOVEMENTS]);
  */
 int main(void)
 {
-	/* b) Chessboard and Position */
-	int board[ROWS][COLUMNS] = {0}, steps[ROWS][COLUMNS] = {0};
-	int horizontal[MOVEMENTS] = {2, 1, -1, -2, -2, -1, 1, 2};
-	int vertical[MOVEMENTS] = {-1, -2, -2, -1, 1, 2, 2, 1};
+        /* b) Chessboard and Position */
+        int board[ROWS][COLUMNS] = {0}, steps[ROWS][COLUMNS] = {0};
+        int horizontal[MOVEMENTS] = {2, 1, -1, -2, -2, -1, 1, 2};
+        int vertical[MOVEMENTS] = {-1, -2, -2, -1, 1, 2, 2, 1};
 
-	int posx[MOVEMENTS] = {0}, posy[MOVEMENTS] = {0};
-	int movenum = 0, currrow = 3, currcol = 4;
-	int countmovements = 1, countpos = 0;
+        int posx[MOVEMENTS] = {0}, posy[MOVEMENTS] = {0};
+        int movenum = 0, currrow = 3, currcol = 4;
+        int countmovements = 1, countpos = 0;
 
-	int k = 0, j = 0;
-	int correctpos[MOVEMENTS] = {0};
+        int k = 0, j = 0;
+        int correctpos[MOVEMENTS] = {0};
 
-	int histogram[1000] = {0};
-	int emptyfields = 0;
+        int histogram[1000] = {0};
+        int emptyfields = 0;
 
-	/* Initialize King */
-	board[currrow][currcol] = 65;
-	srand(time(0));
+        /* Initialize King */
+        board[currrow][currcol] = 65;
+        srand(time(0));
 
-	/* Proccess 1000 Tours */
-	for (int l = 0; l < 1000; ++l) {
-		/* Optional: 1 tour possible */
-		for (countmovements = 1; countmovements <= 64; ++countmovements) {
-			for (int i = 0; i < MOVEMENTS; ++i) {
-				posx[i] = currcol + horizontal[i];
-				posy[i] = currrow + vertical[i];
-				if (possible_move(posx[i], posy[i]) == 1) {
-					if (countpos == 0) {
-					board[posy[i]][posx[i]] = 70;
-					correctpos[k] = 0;
-					++k;
-					} else {
-						board[posy[i]][posx[i]] = countpos;
-						correctpos[k] = countpos;
-					++k;
-					}
-				}
-				++countpos;
-			}
+        /* Proccess 1000 Tours */
+        for (int l = 0; l < 1000; ++l) {
+                /* Optional: 1 tour possible */
+                for (countmovements = 1; countmovements <= 64; ++countmovements) {
+                        for (int i = 0; i < MOVEMENTS; ++i)
+                        {
+                                posx[i] = currcol + horizontal[i];
+                                posy[i] = currrow + vertical[i];
+                                if (possible_move(posx[i], posy[i]) == 1) {
+                                        if (countpos == 0)
+                                        {
+                                                board[posy[i]][posx[i]] = 70;
+                                                correctpos[k] = 0;
+                                                ++k;
+                                        } else {
+                                                board[posy[i]][posx[i]] = countpos;
+                                                correctpos[k] = countpos;
+                                                ++k;
+                                        }
+                                }
+                                ++countpos;
+                        }
 
-			/* Look If Is Posible Movement */
-			movenum = posible_positions(correctpos, k);
-			while (steps[posy[movenum]][posx[movenum]] != 0) {
-				movenum = posible_positions(correctpos, k);
-				++j;
-				if (j < k)
-					break;
-			}
-			steps[currrow][currcol] = countmovements;
+                        /* Look If Is Posible Movement */
+                        movenum = posible_positions(correctpos, k);
+                        while (steps[posy[movenum]][posx[movenum]] != 0) {
+                                movenum = posible_positions(correctpos, k);
+                                ++j;
+                                if (j < k)
+                                        break;
+                        }
+                        steps[currrow][currcol] = countmovements;
 
-			/* Clear Screen */
-			for (int i = 0; i < ROWS; ++i) {
-				for (int j = 0; j < COLUMNS; ++j) {
-					board[i][j] = 0;
-				}
-			}
+                        /* Clear Screen */
+                        for (int i = 0; i < ROWS; ++i) {
+                                for (int j = 0; j < COLUMNS; ++j) {
+                                        board[i][j] = 0;
+                                }
+                        }
 
-			/* Store New Position Introduce to the User*/
-			currcol += horizontal[movenum];
-			currrow += vertical[movenum];
+                        /* Store New Position Introduce to the User*/
+                        currcol += horizontal[movenum];
+                        currrow += vertical[movenum];
 
-			/* Put the K in the Position Choosed */
-			board[currrow][currcol] = 65;
-			countpos = 0;
-			k = 0;
-			j = 0;
-		}
+                        /* Put the K in the Position Choosed */
+                        board[currrow][currcol] = 65;
+                        countpos = 0;
+                        k = 0;
+                        j = 0;
+                }
 
-		/* Count Empty fields */
-		for (int i = 0; i < ROWS; ++i) {
-			for (int j = 0; j < COLUMNS; ++j) {
-				if (steps[i][j] == 0) {
-					++emptyfields;
-				}
-			}
-		}
-		histogram[l] = emptyfields;
-		emptyfields = 0;
+                /* Count Empty fields */
+                for (int i = 0; i < ROWS; ++i) {
+                        for (int j = 0; j < COLUMNS; ++j) {
+                                if (steps[i][j] == 0) {
+                                        ++emptyfields;
+                                }
+                        }
+                }
+                histogram[l] = emptyfields;
+                emptyfields = 0;
 
-		/* Clear Count Movements */
-		for (int i = 0; i < ROWS; ++i) {
-			for (int j = 0; j < COLUMNS; ++j) {
-				steps[i][j] = 0;
-			}
-		}
+                /* Clear Count Movements */
+                for (int i = 0; i < ROWS; ++i) {
+                        for (int j = 0; j < COLUMNS; ++j) {
+                                steps[i][j] = 0;
+                        }
+                }
+        }
 
-	}
+        /* Print Min Position and find the Min Tour*/
+        full_tour(histogram);
+        print_2d(board);
 
-	/* Print Min Position and find the Min Tour*/
-	full_tour(histogram);
-	print_2d(board);
-
-	return (0);
+        return (0);
 }
+
 
 
 /**
@@ -121,11 +126,11 @@ int main(void)
  * @num: (int) Copy of the number to look if is correct
  * @maximum: (int) Size of the maximum of values
  *
- * Return: 1 if the number is correct, otherwise 0
+ * Return: (int) 1 if the number is correct, otherwise 0
  */
 int sanitize_num(int num, int maximum)
 {
-	return ((num <= maximum && num >= 0) ? 1 : 0);
+        return ((num <= maximum && num >= 0) ? 1 : 0);
 }
 
 
@@ -134,37 +139,38 @@ int sanitize_num(int num, int maximum)
  * @newx: (int) Number of the possible position x
  * @newy: (int) Number of the possible position y
  *
- * Return: 1 if the movement is posible, otherwise 0
+ * Return: (int) 1 if the movement is posible, otherwise 0
  */
 int possible_move(int newx, int newy)
 {
-	return(((newx >= 0 && newx <= 7) && (newy >= 0 && newy <= 7)) ? 1 : 0);
+        return (((newx >= 0 && newx <= 7) && (newy >= 0 && newy <= 7)) ? 1 : 0);
 }
 
 
 /**
  * print_2d - Print the chessboard
- * @map: (int) Matrix to prin
+ * @map: (int) Matrix to print
+ *
  */
 void print_2d(int map[ROWS][COLUMNS])
 {
-	printf("    0  1  2  3  4  5  6  7\n");
-	for (size_t i = 0; i < ROWS; ++i) {
-		printf("%ld ", i);
-		for (size_t j = 0; j < COLUMNS; ++j) {
-			if (map[i][j] == 0) {
-				printf("%3s", "-");
-			}
-			if (map[i][j] == 65) {
-				printf("%3s", "K");
-			} else if (map[i][j] >= 1 && map[i][j] <= 64) {
-				printf(" %2d", map[i][j]);
-			}
-			if (map[i][j] == 70)
-				printf("%3s", "0");
-		}
-		putchar(10);
-	}
+        printf("    0  1  2  3  4  5  6  7\n");
+        for (size_t i = 0; i < ROWS; ++i) {
+                printf("%ld ", i);
+                for (size_t j = 0; j < COLUMNS; ++j) {
+                        if (map[i][j] == 0) {
+                                printf("%3s", "-");
+                        }
+                        if (map[i][j] == 65) {
+                                printf("%3s", "K");
+                        } else if (map[i][j] >= 1 && map[i][j] <= 64){
+                                printf(" %2d", map[i][j]);
+                        }
+                        if (map[i][j] == 70)
+                                printf("%3s", "0");
+                }
+                putchar(10);
+        }
 }
 
 
@@ -176,10 +182,10 @@ void print_2d(int map[ROWS][COLUMNS])
  */
 void print_pos(int x[MOVEMENTS], int y[MOVEMENTS])
 {
-	for (size_t i = 0; i < MOVEMENTS; ++i) {
-		printf("Horizonatal[%ld] = %d\tVertical[%ld] = %d\n",
-		       i, x[i], i, y[i]);
-	}
+        for (size_t i = 0; i < MOVEMENTS; ++i) {
+                printf("Horizonatal[%ld] = %d\tVertical[%ld] = %d\n",
+                       i, x[i], i, y[i]);
+        }
 }
 
 
@@ -188,15 +194,15 @@ void print_pos(int x[MOVEMENTS], int y[MOVEMENTS])
  * @pos: (int) Contiguos memory with the possible positions
  * @size: (int) Length of valid values of the array
  *
- * Return: The number to movement
+ * Return: (int) The number to movement
  */
 int posible_positions(int pos[MOVEMENTS], int size)
 {
-	int move = 0;
+        int move = 0;
 
-	move = (rand() % size);
+        move = (rand() % size);
 
-	return (pos[move]);
+        return (pos[move]);
 }
 
 
@@ -206,12 +212,13 @@ int posible_positions(int pos[MOVEMENTS], int size)
  */
 void full_tour(int his[MOVEMENTS])
 {
-	int min = his[0], i = 0;
-	for (; i < 1000; ++i) {
-		if (his == 0)
-			printf("Full Tour\nidx: %d", i);
-		if (his[i] <= min)
-			min = his[i];
-	}
-	printf("Best empty field is.\n\nValue: %d\n", min);
+        int min = his[0], i = 0;
+        for (; i < 1000; ++i)
+        {
+                if (his == 0)
+                        printf("Full Tour\nidx: %d", i);
+                if (his[i] <= min)
+                        min = his[i];
+        }
+        printf("Best empty field is.\n\nValue: %d\n", min);
 }
