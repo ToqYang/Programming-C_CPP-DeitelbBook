001d71d6f52183128abe95b13918a5f04841cc84
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 19:18:38 -0500

    [Feature] Print array in recursion

--- Arrays/sixthirtyfive.c
+++ Arrays/sixthirtyfive.c
@@ -1,31 +1,35 @@
 #include <stdio.h>
 
+
 void print_array(char *str, int size);
+
 
 /**
  * main - (Print an Array) Recursion
  *
- * Return: 0 if is success
+ * Return: (int) 0 if is success
  */
 int main(void)
 {
-	char myname[8] = "Santiago";
+        char myname[8] = "Santiago";
 
-	print_array(myname, 7);
+        print_array(myname, 7);
 
-	return (0);
+        return (0);
 }
+
 
 /**
  * print_array - In mode recursion
  * @str: (char *) String with a message
  * @size: (int) Size of the string without the NULL
+ *
  */
 void print_array(char *str, int size)
 {
-	if (size == -1)
-		return;
+        if (size == -1)
+                return;
 
-	print_array(str, size - 1);
-	printf("%c", str[size]);
+        print_array(str, size - 1);
+        printf("%c", str[size]);
 }
