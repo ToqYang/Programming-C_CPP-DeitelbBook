d7b1dfe3386a02eb1674acb010302b3d6762e847
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 19:17:00 -0500

    [Feature] The Sieve of Eratosthenes

--- Arrays/sixthirty.c
+++ Arrays/sixthirty.c
@@ -1,36 +1,39 @@
 #include <stdio.h>
+
 #define LENGTH 1000
+
 
 void print_arr(int array[LENGTH]);
 
+
 /**
  * main - (The Sieve of Eratosthenes)
+ *
  *        Detect if a number is prime
  *
- * Return: 0 If is success
+ * Return: (int) 0 If is success
  */
 int main(void)
 {
-	int elements[LENGTH] = {0};
+        int elements[LENGTH] = {0};
 
-	/* Initialize Array in 1 */
-	for (int i = 0; i < LENGTH; ++i) {
-		elements[i] = 1;
-	}
+        /* Initialize Array in 1 */
+        for (int i = 0; i < LENGTH; ++i) {
+                elements[i] = 1;
+        }
 
-	/* Delete num to num to verify is prime */
-	for (int i = 2; i < LENGTH; ++i) {
-		for (int j = 2; j < LENGTH; ++j) {
-			if (i % j == 0 && i != j)
-				elements[i] = 0;
-		}
-	}
-	elements[0] = 0;
-	print_arr(elements);
+        /* Delete num to num to verify is prime */
+        for (int i = 2; i < LENGTH; ++i) {
+                for (int j = 2; j < LENGTH; ++j) {
+                        if (i % j == 0 && i != j)
+                                elements[i] = 0;
+                }
+        }
+        elements[0] = 0;
+        print_arr(elements);
 
-	return (0);
+        return (0);
 }
-
 
 
 /**
@@ -39,9 +42,9 @@ int main(void)
  */
 void print_arr(int array[LENGTH])
 {
-	for (int i = 0; i < LENGTH; ++i) {
-		if (array[i] == 1)
-			printf("%d ", i);
-	}
-	puts("");
+        for (int i = 0; i < LENGTH; ++i) {
+                if (array[i] == 1)
+                        printf("%d ", i);
+        }
+        puts("");
 }
