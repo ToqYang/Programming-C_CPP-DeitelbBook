f31104656895c80d01c4ada41d2bf7274ae8daaf
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-30 19:10:30 -0500

    [Feature] Rand

--- Arrays/sixnineteen.c
+++ Arrays/sixnineteen.c
@@ -2,30 +2,31 @@
 #include <stdlib.h>
 #include <time.h>
 
+
 /**
- * (Dice Rolling)
- * main - Simulates the rolling of two dice
+ * main - (Dice Rolling)
  *
- * Return: 0 If is success
+ * Simulates the rolling of two dice
+ *
+ * Return: (int) 0 If is success
  */
-int main (void)
+int main(void)
 {
-	size_t roll1 = 0, roll2 = 0, sum = 0;
-	size_t counter[10] = {0};
+        size_t roll1 = 0, roll2 = 0, sum = 0;
+        size_t counter[10] = {0};
 
-	srand(time(NULL));
+        srand(time(NULL));
 
-	for (size_t i = 0; i < 36000; ++i) {
-		roll1 = 1 + rand() % 6;
-		roll2 = 1 + rand() % 6;
-		sum = roll1 + roll2;
-		++counter[sum - 2];
-	}
+        for (size_t i = 0; i < 36000; ++i) {
+                roll1 = 1 + rand() % 6;
+                roll2 = 1 + rand() % 6;
+                sum = roll1 + roll2;
+                ++counter[sum - 2];
+        }
 
-	puts("Sums   Times\n");
-	for (size_t j = 0, k = 2; j < 10; j++, k++) {
-		printf("  %ld    %ld\n", k, counter[j]);
-	}
+        puts("Sums   Times\n");
+        for (size_t j = 0, k = 2; j < 10; j++, k++)
+                printf("  %ld    %ld\n", k, counter[j]);
 
-	return (0);
+        return (0);
 }
