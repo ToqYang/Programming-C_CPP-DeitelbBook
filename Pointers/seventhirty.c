006663f773b4be8702a836e5641b8a0780316d02
Santiago Yanguas <santitoya-2001@outlook.com>
2020-07-31 14:45:16 -0500

    [Feature] Func ptr

--- Pointers/seventhirty.c
+++ Pointers/seventhirty.c
@@ -1,49 +1,54 @@
 #include <stdio.h>
 #include <math.h>
 
+
 void circunference_circle(double *radio);
 void area_circle(double *radio);
 void volume_sphere(double *radio);
+
 
 /**
  * main - (Calculating Circle Circumference,
  *        Circle Area or Sphere Volume Using Function Pointers)
  *
- * Return: 0 if is success
+ * Return: (int) 0 if is success
  */
 int main(void)
 {
-	int opt = 0;
-	double radio = 0;
-	void (*circleop[3])(double *) = {circunference_circle, area_circle, volume_sphere};
+        int opt = 0;
+        double radio = 0;
+        void (*circleop[3])(double *) = {circunference_circle, area_circle, volume_sphere};
 
-	while (opt != 3) {
-		puts("Enter a choice:");
-		puts("    0  Circunference");
-		puts("    1  Circle Area");
-		puts("    2  Sphere Volume");
-		puts("    3  End Program");
+        while (opt != 3) {
+                puts("Enter a choice:");
+                puts("    0  Circunference");
+                puts("    1  Circle Area");
+                puts("    2  Sphere Volume");
+                puts("    3  End Program");
 
-		scanf("%d", &opt);
+                scanf("%d", &opt);
 
-		if (opt != 3) {
-			puts("Enter The Radio");
-			scanf("%lf", &radio);
-			(*circleop[opt])(&radio);
-		}
-	}
-	return (0);
+                if (opt != 3) {
+                        puts("Enter The Radio");
+                        scanf("%lf", &radio);
+                        (*circleop[opt])(&radio);
+                }
+        }
+        return (0);
 }
+
 
 
 /**
  * circunference_circle - Calculate circunference based in the radio
+ *
  * @radio: (double *) It is the straight line that can be drawn
  *               from the center of a circle to the circunference
+ *
  */
 void circunference_circle(double *radio)
 {
-	printf("\nThe circuference is: %.2lf\n\n", (2 * M_PI * *radio));
+        printf("\nThe circuference is: %.2lf\n\n", (2 * M_PI * *radio));
 }
 
 
@@ -54,7 +59,7 @@ void circunference_circle(double *radio)
  */
 void area_circle(double *radio)
 {
-	printf("\nArea of the is: %.2lf\n\n", (M_PI * pow(*radio, 2)));
+        printf("\nArea of the is: %.2lf\n\n", (M_PI * pow(*radio, 2)));
 }
 
 
@@ -65,5 +70,5 @@ void area_circle(double *radio)
  */
 void volume_sphere(double *radio)
 {
-	printf("\nVolume of a sphere: %.2lf\n\n", ((4 / 3) * M_PI * pow(*radio, 3)));
+        printf("\nVolume of a sphere: %.2lf\n\n", ((4 / 3) * M_PI * pow(*radio, 3)));
 }
