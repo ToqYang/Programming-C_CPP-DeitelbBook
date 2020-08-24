b953f7e1abead216f80f03e86cb6f0ee8b6fa691
Santiago Yanguas <santitoya-2001@outlook.com>
2020-08-24 15:33:38 -0500

    [Feature] Duplicates binary tree

--- Data-Structures/twelvesixteen.c
+++ Data-Structures/twelvesixteen.c
@@ -1,13 +1,113 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct treeNode {
+    struct treeNode *leftPtr; /* pointer to left subtree */
+    int data; /* node value */
+    struct treeNode *rightPtr; /* pointer to right subtree */
+};
+
+
+typedef struct treeNode TreeNode; /* synonym for struct treeNode */
+typedef TreeNode *TreeNodePtr; /* synonym for TreeNode* */
+
+void insertNode(TreeNodePtr *treePtr, int value);
+void inOrder(TreeNodePtr treePtr);
+void preOrder(TreeNodePtr treePtr);
+void postOrder(TreeNodePtr treePtr);
 
 
 /**
- * main - Follow Steps
+ * main - (Allowing Duplicates in a Binary Tree)
  *
- * Return: 0 if is success
+ * Return: (int) 0 if is success
  */
 int main(void)
 {
+    TreeNodePtr rootPtr = NULL; /* tree initially empty */
 
-	return (0);
+    srand(time(NULL));
+    puts("The numbers being placed in the tree are:");
+
+    /* insert random values between 0 and 14 in the tree*/
+    for (unsigned int i = 1; i <= 10; ++i) {
+        int item = rand() % 15;
+        printf("%3d", item);
+        insertNode(&rootPtr, item);
+    }
+
+    /* traverse the tree preOrder */
+    puts("\n\nThe preOrder traversal is:");
+    preOrder(rootPtr);
+    /* traverse the tree inOrder */
+    puts("\n\nThe inOrder traversal is:");
+    inOrder(rootPtr);
+    /* traverse the tree postOrder */
+    puts("\n\nThe postOrder traversal is:");
+    postOrder(rootPtr);
+
+    return (0);
+}
+
+
+/* insert node into tree */
+void insertNode(TreeNodePtr *treePtr, int value)
+{
+    /* if tree is empty */
+    if (*treePtr == NULL) {
+        *treePtr = malloc(sizeof(TreeNode));
+        /* if memory was allocated, then assign data */
+        if (*treePtr != NULL) {
+            (*treePtr)->data = value;
+            (*treePtr)->leftPtr = NULL;
+            (*treePtr)->rightPtr = NULL;
+        } else {
+            printf("%d not inserted. No memory available.\n", value);
+        }
+    }
+    else { /* tree is not empty */
+    /* data to insert is less than data in current node */
+        if (value <= (*treePtr)->data) {
+            insertNode(&((*treePtr)->leftPtr), value);
+        } else if (value >= (*treePtr)->data) {
+            insertNode(&((*treePtr)->rightPtr), value);
+        } else { /* duplicate data value ignored */
+            printf("%s", "dup");
+        }
+        /* SOLUTION change conditions < and > to >= <= */
+    }
+}
+
+
+/* begin inorder traversal of tree */
+void inOrder(TreeNodePtr treePtr)
+{
+    /* if tree is not empty, then traverse */
+    if (treePtr != NULL) {
+        inOrder(treePtr->leftPtr);
+        printf("%3d", treePtr->data);
+        inOrder(treePtr->rightPtr);
+    }
+}
+
+/* begin preorder traversal of tree */
+void preOrder(TreeNodePtr treePtr)
+{
+    /* if tree is not empty, then traverse */
+    if (treePtr != NULL) {
+        printf("%3d", treePtr->data);
+        preOrder(treePtr->leftPtr);
+        preOrder(treePtr->rightPtr);
+    }
+}
+
+/* begin postorder traversal of tree */
+void postOrder(TreeNodePtr treePtr) {
+    /* if tree is not empty, then traverse */
+    if (treePtr != NULL) {
+        postOrder(treePtr->leftPtr);
+        postOrder(treePtr->rightPtr);
+        printf("%3d", treePtr->data);
+    }
 }
